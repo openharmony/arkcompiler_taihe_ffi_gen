589e4167c6aaee2bf0fb12b0d3309fa750a0ac5c
Jemtaly <Jemtaly@outlook.com>
2024-09-10 12:53:05 +0800

    compiler: fix symbol and namespace conflict checking
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- compiler/semantic_analysis.py
+++ compiler/semantic_analysis.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from exceptions import *
 from TaiheAST import TaiheAST
+from TaiheVisitor import TaiheVisitor
 
 
 @dataclass
@@ -25,18 +26,20 @@ def semantic_analysis(packages: list[PackageInput]) -> list[PackageOutput]:
         if other_path != package.path:
             raise PackageNameConflictError(package.path, other_path)
     # Generate namespace tree
+    namespaces = {}
     namespace_tree = {}
     for package in packages:
         namespace = namespace_tree
         for part in package.name:
             namespace = namespace.setdefault(part, {})
+        namespaces[package.name] = namespace
     # Check for symbol collisions, not considering `use` statements, generate symbol tables
     local_symbol_tables = {}
     for package in packages:
         local_symbol_table = local_symbol_tables.setdefault(package.name, {})
         for field in package.spec.fields:
             symbol = field.name.text
-            if symbol in namespace:
+            if symbol in namespaces[package.name]:
                 raise SymbolCollisionWithNamespaceError(package.path, package.name, field)
             first = local_symbol_table.setdefault(symbol, field)
             if first is not field:
