ab29e3a1b021b3f2dc47218cd4b1960249ff15bc
seasalt <654765554@qq.com>
2024-09-19 09:50:42 +0800

    compiler: hide the implementation detail about ANTLR(modify format)
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- compiler/taihe/parse/__init__.py
+++ compiler/taihe/parse/__init__.py
@@ -1,2 +1,2 @@
-from parse.antlr.TaiheAST import TaiheAST as ast  # noqa: F401
-from parse.antlr.TaiheVisitor import TaiheVisitor as Visitor  # noqa: F401
+from taihe.parse.antlr.TaiheAST import TaiheAST as ast  # noqa: F401
+from taihe.parse.antlr.TaiheVisitor import TaiheVisitor as Visitor  # noqa: F401
