aeef3c5444efc2962086bccdd1102964732b42f6
Jemtaly <Jemtaly@outlook.com>
2024-09-20 17:00:15 +0800

    compiler: add into_abi and from_abi
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- compiler/taihe/code_generation.py
+++ compiler/taihe/code_generation.py
@@ -4,7 +4,7 @@ from taihe.parse import Visitor, ast
 
 
 class File:
-    def __init__(self, is_header: bool = True):
+    def __init__(self, is_header):
         self.is_header = is_header
         self.headers: set[str] = set()
         self.code = StringIO()
@@ -17,11 +17,13 @@ class File:
                 dst.write(f'#include "{header}"\n')
             dst.write(self.code.getvalue())
 
-    def update(self, code: str):
+    def write(self, code: str):
         self.code.write(code)
 
-    def include(self, *header: str):
-        self.headers.update(header)
+    def include(self, *headers: str | None):
+        for header in headers:
+            if header is not None:
+                self.headers.add(header)
 
 
 class CodeGenerator(Visitor):
@@ -35,52 +37,49 @@ class CodeGenerator(Visitor):
         raise NotImplementedError
 
     def visit_BasicType(self, node: ast.BasicType, cpp: bool, param: bool):
-        if node.name.text == "i8":
-            return "int8_t"
-        if node.name.text == "i16":
-            return "int16_t"
-        if node.name.text == "i32":
-            return "int32_t"
-        if node.name.text == "i64":
-            return "int64_t"
-        if node.name.text == "u8":
-            return "uint8_t"
-        if node.name.text == "u16":
-            return "uint16_t"
-        if node.name.text == "u32":
-            return "uint32_t"
-        if node.name.text == "u64":
-            return "uint64_t"
-        if node.name.text == "f32":
-            return "float"
-        if node.name.text == "f64":
-            return "double"
         if node.name.text == "bool":
-            return "bool"
+            return "bool", None
+        if node.name.text == "f32":
+            return "float", None
+        if node.name.text == "f64":
+            return "double", None
+        if node.name.text == "i8":
+            return "int8_t", None
+        if node.name.text == "i16":
+            return "int16_t", None
+        if node.name.text == "i32":
+            return "int32_t", None
+        if node.name.text == "i64":
+            return "int64_t", None
+        if node.name.text == "u8":
+            return "uint8_t", None
+        if node.name.text == "u16":
+            return "uint16_t", None
+        if node.name.text == "u32":
+            return "uint32_t", None
+        if node.name.text == "u64":
+            return "uint64_t", None
         raise NotImplementedError
 
     def visit_UserType(self, node: ast.UserType, cpp: bool, param: bool):
         assert isinstance(node.pkname, ast.PackageName)
+        type_name = node.name.text
         pktupl = tuple(token.text for token in node.pkname.parts)
-        if not cpp:
-            return "__".join(pktupl) + "__" + node.name.text
+        type_basename = ".".join(pktupl) + "." + type_name
+        if cpp:
+            return "::".join(pktupl) + "::" + type_name, type_basename + ".abi.hpp"
         else:
-            return "::".join(pktupl) + "::" + node.name.text
+            return "__".join(pktupl) + "__" + type_name, type_basename + ".abi.h"
 
     def visit_TypeWithSpecifier(
         self, node: ast.TypeWithSpecifier, cpp: bool, param: bool
     ):
-        text = self.visit(node.type, cpp, param)
+        type, header = self.visit(node.type, cpp, param)
         if node.const:
-            text += " const"
+            type += " const"
         if node.ref:
-            text += "&" if cpp else "*"
-        return text
-
-    def visit_Parameter(self, node: ast.Parameter, cpp: bool):
-        return (
-            self.visit(node.type_with_specifier, cpp, param=True) + " " + node.name.text
-        )
+            type += "&" if cpp else "*"
+        return type, header
 
     def visit_Specification(self, node: ast.Specification):
         basename = ".".join(self.pktupl)
@@ -90,18 +89,21 @@ class CodeGenerator(Visitor):
         impl_hpp_name = basename + ".impl.hpp"
 
         if self.author or self.user:
-            abi_h = self.files.setdefault(abi_h_name, File())
-            abi_h.update('#include "taihe/common.h"\n')
+            abi_h = self.files.setdefault(abi_h_name, File(is_header=True))
+            abi_h.include("taihe/common.h")
 
         if self.user:
-            abi_hpp = self.files.setdefault(abi_hpp_name, File())
+            abi_hpp = self.files.setdefault(abi_hpp_name, File(is_header=True))
+            abi_hpp.include("core/common.hpp")
             abi_hpp.include(abi_h_name)
 
         if self.author:
-            impl_h = self.files.setdefault(impl_h_name, File())
+            impl_h = self.files.setdefault(impl_h_name, File(is_header=True))
+            impl_h.include("taihe/common.h")
             impl_h.include(abi_h_name)
 
-            impl_hpp = self.files.setdefault(impl_hpp_name, File())
+            impl_hpp = self.files.setdefault(impl_hpp_name, File(is_header=True))
+            impl_hpp.include("core/common.hpp")
             impl_hpp.include(abi_h_name)
 
         for field in node.fields:
@@ -109,6 +111,7 @@ class CodeGenerator(Visitor):
 
     def visit_Function(self, node: ast.Function):
         func_name = node.name.text
+        func_abi_name = "__".join(self.pktupl) + "__" + func_name
 
         basename = ".".join(self.pktupl)
         abi_h_name = basename + ".abi.h"
@@ -118,99 +121,74 @@ class CodeGenerator(Visitor):
 
         namespace = "::".join(self.pktupl)
 
-        func_abi_name = "__".join(self.pktupl) + "__" + func_name
-        cpp_args_from_abi = ", ".join(
-            ("*" if param.type_with_specifier.ref else "") + param.name.text
-            for param in node.parameters
-        )
-        abi_args_from_cpp = ", ".join(
-            ("&" if param.type_with_specifier.ref else "") + param.name.text
-            for param in node.parameters
-        )
-        abi_args = ", ".join(param.name.text for param in node.parameters)
-        cpp_params = ", ".join(
-            [self.visit(param, cpp=True) for param in node.parameters]
-        )
-        abi_params = ", ".join(
-            [self.visit(param, cpp=False) for param in node.parameters]
-        )
+        args_from_abi = []
+        args_into_abi = []
+        cpp_params = []
+        abi_params = []
+        cpp_param_headers = []
+        abi_param_headers = []
+        for param in node.parameters:
+            cpp_param_type, cpp_param_header = self.visit(
+                param.type_with_specifier, cpp=True, param=True
+            )
+            abi_param_type, abi_param_header = self.visit(
+                param.type_with_specifier, cpp=False, param=True
+            )
+            param_name = param.name.text
+            cpp_param_headers.append(cpp_param_header)
+            abi_param_headers.append(abi_param_header)
+            args_from_abi.append(f"from_abi<{cpp_param_type}>({param_name})")
+            args_into_abi.append(f"into_abi<{cpp_param_type}>({param_name})")
+            cpp_params.append(f"{cpp_param_type} {param_name}")
+            abi_params.append(f"{abi_param_type} {param_name}")
+        args_from_abi_str = ", ".join(args_from_abi)
+        args_into_abi_str = ", ".join(args_into_abi)
+        cpp_params_str = ", ".join(cpp_params)
+        abi_params_str = ", ".join(abi_params)
 
-        return_count = len(node.return_types)
-        if return_count == 0:
-            cpp_return_type = "void"
-            abi_return_type = "void"
-        elif return_count == 1:
-            cpp_return_type = self.visit(node.return_types[0], cpp=True, param=False)
-            abi_return_type = self.visit(node.return_types[0], cpp=False, param=False)
+        if len(node.return_types) == 0:
+            cpp_return_type, cpp_return_header = "void", None
+            abi_return_type, abi_return_header = "void", None
+            return_from_abi = f""
+            return_into_abi = f""
+        elif len(node.return_types) == 1:
+            cpp_return_type, cpp_return_header = self.visit(
+                node.return_types[0], cpp=True, param=False
+            )
+            abi_return_type, abi_return_header = self.visit(
+                node.return_types[0], cpp=False, param=False
+            )
+            return_from_abi = f"from_abi<{cpp_return_type}>"
+            return_into_abi = f"into_abi<{cpp_return_type}>"
         else:
             raise NotImplementedError
-        if return_count == 1 and node.return_types[0].ref:
-            cpp_rv_from_abi = "*"
-            abi_rv_from_cpp = "&"
-        else:
-            cpp_rv_from_abi = ""
-            abi_rv_from_cpp = ""
-
-        types = [
-            param.type_with_specifier.type
-            for param in node.parameters
-            if isinstance(param.type_with_specifier.type, ast.UserType)
-        ] + [
-            return_type.type
-            for return_type in node.return_types
-            if isinstance(return_type.type, ast.UserType)
-        ]
 
         if self.author or self.user:
             abi_h = self.files[abi_h_name]
-            for type in types:
-                assert isinstance(type.pkname, ast.PackageName)
-                pktupl = tuple(token.text for token in type.pkname.parts)
-                text = type.name.text
-                user_type_abi_h_name = ".".join(pktupl) + "." + text + ".abi.h"
-                abi_h.include(user_type_abi_h_name)
-            abi_h.update(
-                f"TH_EXPORT {abi_return_type} {func_abi_name}({abi_params});\n"
-            )
+            abi_h.include(*abi_param_headers, abi_return_header)
+            abi_h.write(f"TH_EXPORT {abi_return_type} {func_abi_name}({abi_params_str});\n")
 
         if self.user:
             abi_hpp = self.files[abi_hpp_name]
-            for type in types:
-                assert isinstance(type.pkname, ast.PackageName)
-                pktupl = tuple(token.text for token in type.pkname.parts)
-                text = type.name.text
-                user_type_abi_hpp_name = ".".join(pktupl) + "." + text + ".abi.hpp"
-                abi_hpp.include(user_type_abi_hpp_name)
-            abi_hpp.update(f"namespace {namespace} {{\n")
-            abi_hpp.update(f"inline {cpp_return_type} {func_name}({cpp_params}) {{\n")
-            abi_hpp.update(
-                f"    return {cpp_rv_from_abi}{func_abi_name}({abi_args_from_cpp});\n"
-            )
-            abi_hpp.update("}\n")
-            abi_hpp.update("}\n")
+            abi_hpp.include(*cpp_param_headers, cpp_return_header)
+            abi_hpp.write(f"namespace {namespace} {{\n")
+            abi_hpp.write(f"inline {cpp_return_type} {func_name}({cpp_params_str}) {{\n")
+            abi_hpp.write(f"    return {return_from_abi}({func_abi_name}({args_into_abi_str}));\n")
+            abi_hpp.write("}\n")
+            abi_hpp.write("}\n")
 
         if self.author:
             impl_h = self.files[impl_h_name]
-            impl_h.update(f"#define TH_EXPORT_C_API_{func_name}(_f) \\\n")
-            impl_h.update(f"    {abi_return_type} {func_abi_name}({abi_params}) {{\\\n")
-            impl_h.update(f"        return _f({abi_args}); \\\n")
-            impl_h.update("    }\n")
+            impl_h.include(*abi_param_headers, abi_return_header)
+            impl_h.write(f"#define TH_EXPORT_C_API_{func_name}(_func) \\\n")
+            impl_h.write(f"    {abi_return_type} {func_abi_name}({abi_params_str}) __attribute__((alias(#_func)));\n")
 
             impl_hpp = self.files[impl_hpp_name]
-            for type in types:
-                assert isinstance(type.pkname, ast.PackageName)
-                pktupl = tuple(token.text for token in type.pkname.parts)
-                text = type.name.text
-                user_type_abi_hpp_name = ".".join(pktupl) + "." + text + ".abi.hpp"
-                impl_hpp.include(user_type_abi_hpp_name)
-            impl_hpp.update(f"#define TH_EXPORT_CPP_API_{func_name}(_f) \\\n")
-            impl_hpp.update(
-                f"    {abi_return_type} {func_abi_name}({abi_params}) {{\\\n"
-            )
-            impl_hpp.update(
-                f"        return {abi_rv_from_cpp}_f({cpp_args_from_abi}); \\\n"
-            )
-            impl_hpp.update("    }\n")
+            impl_hpp.include(*cpp_param_headers, cpp_return_header)
+            impl_hpp.write(f"#define TH_EXPORT_CPP_API_{func_name}(_func) \\\n")
+            impl_hpp.write(f"    {abi_return_type} {func_abi_name}({abi_params_str}) {{\\\n")
+            impl_hpp.write(f"        return {return_from_abi}(_func({args_from_abi_str})); \\\n")
+            impl_hpp.write("    }\n")
 
     def visit_Struct(self, node: ast.Struct):
         struct_name = node.name.text
@@ -218,41 +196,75 @@ class CodeGenerator(Visitor):
         basename = ".".join(self.pktupl)
         abi_h_name = basename + ".abi.h"
         abi_hpp_name = basename + ".abi.hpp"
-        struct_abi_h_name = basename + "." + struct_name + ".abi.h"
-        struct_abi_hpp_name = basename + "." + struct_name + ".abi.hpp"
+        struct_basename = basename + "." + struct_name
+        struct_abi_h_name = struct_basename + ".abi.h"
+        struct_abi_hpp_name = struct_basename + ".abi.hpp"
 
         namespace = "::".join(self.pktupl)
 
         struct_abi_name = "__".join(self.pktupl) + "__" + struct_name
-
-        types = [
-            field.type for field in node.fields if isinstance(field.type, ast.UserType)
-        ]
+        struct_cpp_name = "::".join(self.pktupl) + "::" + struct_name
 
         if self.author or self.user:
-            struct_abi_h = self.files.setdefault(struct_abi_h_name, File())
+            struct_abi_h = self.files.setdefault(
+                struct_abi_h_name, File(is_header=True)
+            )
             struct_abi_h.include("taihe/common.h")
-            for type in types:
-                assert isinstance(type.pkname, ast.PackageName)
-                pktupl = tuple(token.text for token in type.pkname.parts)
-                text = type.name.text
-                user_type_abi_h_name = ".".join(pktupl) + "." + text + ".abi.h"
-                struct_abi_h.include(user_type_abi_h_name)
-            struct_abi_h.update(f"typedef struct {struct_abi_name} {{\n")
+            struct_abi_h.write(f"typedef struct {struct_abi_name} {{\n")
             for field in node.fields:
-                type = self.visit(field.type, cpp=False, param=False)
+                type, header = self.visit(field.type, cpp=False, param=False)
                 name = field.name.text
-                struct_abi_h.update(f"    {type} {name};\n")
-            struct_abi_h.update(f"}} {struct_abi_name};\n")
+                struct_abi_h.write(f"    {type} {name};\n")
+                struct_abi_h.include(header)
+            struct_abi_h.write(f"}} {struct_abi_name};\n")
 
             abi_h = self.files[abi_h_name]
             abi_h.include(struct_abi_h_name)
 
-            struct_abi_hpp = self.files.setdefault(struct_abi_hpp_name, File())
+            struct_abi_hpp = self.files.setdefault(
+                struct_abi_hpp_name, File(is_header=True)
+            )
+            struct_abi_hpp.include("core/common.hpp")
             struct_abi_hpp.include(struct_abi_h_name)
-            struct_abi_hpp.update(f"namespace {namespace} {{\n")
-            struct_abi_hpp.update(f"using {struct_name} = {struct_abi_name};\n")
-            struct_abi_hpp.update("}\n")
+            struct_abi_hpp.write(f"namespace {namespace} {{\n")
+            struct_abi_hpp.write(f"using {struct_name} = {struct_abi_name};\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"struct as_abi<{struct_cpp_name}> {{\n")
+            struct_abi_hpp.write(f"    using type = {struct_abi_name};\n")
+            struct_abi_hpp.write("};\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_abi_name} into_abi({struct_cpp_name} _val) {{\n")
+            struct_abi_hpp.write("    return _val;\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_cpp_name} from_abi({struct_abi_name} _val) {{\n")
+            struct_abi_hpp.write("    return _val;\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"struct as_abi<{struct_cpp_name}&> {{\n")
+            struct_abi_hpp.write(f"    using type = {struct_abi_name}*;\n")
+            struct_abi_hpp.write("};\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_abi_name} *into_abi({struct_cpp_name} &_val) {{\n")
+            struct_abi_hpp.write("    return &_val;\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_cpp_name} &from_abi({struct_abi_name} *_val) {{\n")
+            struct_abi_hpp.write("    return *_val;\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"struct as_abi<{struct_cpp_name} const&> {{\n")
+            struct_abi_hpp.write(f"    using type = {struct_abi_name} const*;\n")
+            struct_abi_hpp.write("};\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_abi_name} const *into_abi({struct_cpp_name} const &_val) {{\n")
+            struct_abi_hpp.write("    return &_val;\n")
+            struct_abi_hpp.write("}\n")
+            struct_abi_hpp.write("template<>\n")
+            struct_abi_hpp.write(f"inline {struct_cpp_name} const &from_abi({struct_abi_name} const *_val) {{\n")
+            struct_abi_hpp.write("    return *_val;\n")
+            struct_abi_hpp.write("}\n")
 
         if self.user:
             abi_hpp = self.files[abi_hpp_name]
---
+++ runtime/include/core/common.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+extern "C" {
+#include <taihe/common.h>
+}
+
+
+template<typename cpp_t> struct as_abi {};
+
+template<typename cpp_t>
+using as_abi_t = typename as_abi<cpp_t>::type;
+
+template<typename cpp_t> as_abi_t<cpp_t> into_abi(cpp_t val);
+template<typename cpp_t> cpp_t from_abi(as_abi_t<cpp_t> val);
+
+template<> struct as_abi<int8_t  > { using type = int8_t  ; };
+template<> struct as_abi<int16_t > { using type = int16_t ; };
+template<> struct as_abi<int32_t > { using type = int32_t ; };
+template<> struct as_abi<int64_t > { using type = int64_t ; };
+template<> struct as_abi<uint8_t > { using type = uint8_t ; };
+template<> struct as_abi<uint16_t> { using type = uint16_t; };
+template<> struct as_abi<uint32_t> { using type = uint32_t; };
+template<> struct as_abi<uint64_t> { using type = uint64_t; };
+template<> struct as_abi<bool    > { using type = bool    ; };
+template<> struct as_abi<float   > { using type = float   ; };
+template<> struct as_abi<double  > { using type = double  ; };
+template<> inline int8_t   into_abi(int8_t   _val) { return _val; }
+template<> inline int16_t  into_abi(int16_t  _val) { return _val; }
+template<> inline int32_t  into_abi(int32_t  _val) { return _val; }
+template<> inline int64_t  into_abi(int64_t  _val) { return _val; }
+template<> inline uint8_t  into_abi(uint8_t  _val) { return _val; }
+template<> inline uint16_t into_abi(uint16_t _val) { return _val; }
+template<> inline uint32_t into_abi(uint32_t _val) { return _val; }
+template<> inline uint64_t into_abi(uint64_t _val) { return _val; }
+template<> inline bool     into_abi(bool     _val) { return _val; }
+template<> inline float    into_abi(float    _val) { return _val; }
+template<> inline double   into_abi(double   _val) { return _val; }
+template<> inline int8_t   from_abi(int8_t   _val) { return _val; }
+template<> inline int16_t  from_abi(int16_t  _val) { return _val; }
+template<> inline int32_t  from_abi(int32_t  _val) { return _val; }
+template<> inline int64_t  from_abi(int64_t  _val) { return _val; }
+template<> inline uint8_t  from_abi(uint8_t  _val) { return _val; }
+template<> inline uint16_t from_abi(uint16_t _val) { return _val; }
+template<> inline uint32_t from_abi(uint32_t _val) { return _val; }
+template<> inline uint64_t from_abi(uint64_t _val) { return _val; }
+template<> inline bool     from_abi(bool     _val) { return _val; }
+template<> inline float    from_abi(float    _val) { return _val; }
+template<> inline double   from_abi(double   _val) { return _val; }
+
+template<> struct as_abi<int8_t   &> { using type = int8_t   *; };
+template<> struct as_abi<int16_t  &> { using type = int16_t  *; };
+template<> struct as_abi<int32_t  &> { using type = int32_t  *; };
+template<> struct as_abi<int64_t  &> { using type = int64_t  *; };
+template<> struct as_abi<uint8_t  &> { using type = uint8_t  *; };
+template<> struct as_abi<uint16_t &> { using type = uint16_t *; };
+template<> struct as_abi<uint32_t &> { using type = uint32_t *; };
+template<> struct as_abi<uint64_t &> { using type = uint64_t *; };
+template<> struct as_abi<bool     &> { using type = bool     *; };
+template<> struct as_abi<float    &> { using type = float    *; };
+template<> struct as_abi<double   &> { using type = double   *; };
+template<> inline int8_t   *into_abi(int8_t   &_val) { return &_val; }
+template<> inline int16_t  *into_abi(int16_t  &_val) { return &_val; }
+template<> inline int32_t  *into_abi(int32_t  &_val) { return &_val; }
+template<> inline int64_t  *into_abi(int64_t  &_val) { return &_val; }
+template<> inline uint8_t  *into_abi(uint8_t  &_val) { return &_val; }
+template<> inline uint16_t *into_abi(uint16_t &_val) { return &_val; }
+template<> inline uint32_t *into_abi(uint32_t &_val) { return &_val; }
+template<> inline uint64_t *into_abi(uint64_t &_val) { return &_val; }
+template<> inline bool     *into_abi(bool     &_val) { return &_val; }
+template<> inline float    *into_abi(float    &_val) { return &_val; }
+template<> inline double   *into_abi(double   &_val) { return &_val; }
+template<> inline int8_t   &from_abi(int8_t   *_val) { return *_val; }
+template<> inline int16_t  &from_abi(int16_t  *_val) { return *_val; }
+template<> inline int32_t  &from_abi(int32_t  *_val) { return *_val; }
+template<> inline int64_t  &from_abi(int64_t  *_val) { return *_val; }
+template<> inline uint8_t  &from_abi(uint8_t  *_val) { return *_val; }
+template<> inline uint16_t &from_abi(uint16_t *_val) { return *_val; }
+template<> inline uint32_t &from_abi(uint32_t *_val) { return *_val; }
+template<> inline uint64_t &from_abi(uint64_t *_val) { return *_val; }
+template<> inline bool     &from_abi(bool     *_val) { return *_val; }
+template<> inline float    &from_abi(float    *_val) { return *_val; }
+template<> inline double   &from_abi(double   *_val) { return *_val; }
+
+template<> struct as_abi<int8_t   const &> { using type = int8_t   const *; };
+template<> struct as_abi<int16_t  const &> { using type = int16_t  const *; };
+template<> struct as_abi<int32_t  const &> { using type = int32_t  const *; };
+template<> struct as_abi<int64_t  const &> { using type = int64_t  const *; };
+template<> struct as_abi<uint8_t  const &> { using type = uint8_t  const *; };
+template<> struct as_abi<uint16_t const &> { using type = uint16_t const *; };
+template<> struct as_abi<uint32_t const &> { using type = uint32_t const *; };
+template<> struct as_abi<uint64_t const &> { using type = uint64_t const *; };
+template<> struct as_abi<bool     const &> { using type = bool     const *; };
+template<> struct as_abi<float    const &> { using type = float    const *; };
+template<> struct as_abi<double   const &> { using type = double   const *; };
+template<> inline int8_t   const *into_abi(int8_t   const &_val) { return &_val; }
+template<> inline int16_t  const *into_abi(int16_t  const &_val) { return &_val; }
+template<> inline int32_t  const *into_abi(int32_t  const &_val) { return &_val; }
+template<> inline int64_t  const *into_abi(int64_t  const &_val) { return &_val; }
+template<> inline uint8_t  const *into_abi(uint8_t  const &_val) { return &_val; }
+template<> inline uint16_t const *into_abi(uint16_t const &_val) { return &_val; }
+template<> inline uint32_t const *into_abi(uint32_t const &_val) { return &_val; }
+template<> inline uint64_t const *into_abi(uint64_t const &_val) { return &_val; }
+template<> inline bool     const *into_abi(bool     const &_val) { return &_val; }
+template<> inline float    const *into_abi(float    const &_val) { return &_val; }
+template<> inline double   const *into_abi(double   const &_val) { return &_val; }
+template<> inline int8_t   const &from_abi(int8_t   const *_val) { return *_val; }
+template<> inline int16_t  const &from_abi(int16_t  const *_val) { return *_val; }
+template<> inline int32_t  const &from_abi(int32_t  const *_val) { return *_val; }
+template<> inline int64_t  const &from_abi(int64_t  const *_val) { return *_val; }
+template<> inline uint8_t  const &from_abi(uint8_t  const *_val) { return *_val; }
+template<> inline uint16_t const &from_abi(uint16_t const *_val) { return *_val; }
+template<> inline uint32_t const &from_abi(uint32_t const *_val) { return *_val; }
+template<> inline uint64_t const &from_abi(uint64_t const *_val) { return *_val; }
+template<> inline bool     const &from_abi(bool     const *_val) { return *_val; }
+template<> inline float    const &from_abi(float    const *_val) { return *_val; }
+template<> inline double   const &from_abi(double   const *_val) { return *_val; }
