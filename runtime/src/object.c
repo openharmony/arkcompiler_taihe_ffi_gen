c3e0b2346b295d230f06ee3dcc57ab172920f080
Jemtaly <Jemtaly@outlook.com>
2024-10-26 11:03:37 +0800

    rt/object: rename functions; fix export
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- runtime/src/object.c
+++ runtime/src/object.c
@@ -1,24 +1,23 @@
 #include <taihe/common.h>
 #include <taihe/object.abi.h>
 
-
-// dynamic_cast
-// yes! return ptr
-// no! return NULL
-inline void* GetVtptr(struct TypeInfo* typeInfo_ptr, const void* id) {
-  size_t len = typeInfo_ptr->len;
+// success: return ptr
+// failed: return NULL
+void* tobj_get_pvtbl(struct TypeInfo* rtti_ptr, const void* id) {
+  size_t len = rtti_ptr->len;
   for (int i = 0; i < len; ++i) {
-    if ((typeInfo_ptr->idmap[i].id) == id) {
-      return typeInfo_ptr->idmap[i].vtableptr;
+    if ((rtti_ptr->idmap[i].id) == id) {
+      return rtti_ptr->idmap[i].vtbl_ptr;
     }
   }
   return NULL;
 }
 
-int Convert(struct THObject src, struct THObject *dst, const void* id) {
-  void* vtptr = GetVtptr(src.data_ptr->ti_ptr, id);
+// dynamic_cast
+int tobj_dynamic_cast(struct TObject src, struct TObject *dst, const void* id) {
+  void* vtptr = tobj_get_pvtbl(src.data_ptr->rtti_ptr, id);
   if (vtptr) {
-    dst->vtableptr = vtptr;
+    dst->vtbl_ptr = vtptr;
     dst->data_ptr = src.data_ptr;
     return 1;
   }
