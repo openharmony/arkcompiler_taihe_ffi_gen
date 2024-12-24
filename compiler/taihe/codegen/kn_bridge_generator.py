f116a5824797f02c3c3ad7158011185abe5c3c0a
Lyujiayi654 <654765554@qq.com>
2024-12-24 17:32:58 +0800

    kn_bridge: add konan function logic
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- compiler/taihe/codegen/kn_bridge_generator.py
+++ compiler/taihe/codegen/kn_bridge_generator.py
@@ -1,23 +1,15 @@
-# from io import StringIO
-# from os import makedirs, path
-# from pathlib import Path
 from typing import Any
 
 from typing_extensions import override
 
 from taihe.codegen.generator import COutputBuffer
 
-# from taihe.codegen.generator import ABIFuncBaseDeclInfo, COutputBuffer
 from taihe.codegen.mangle import DeclKind, encode
 from taihe.semantics.declarations import (
-    # EnumDecl,
-    # EnumItemDecl,
     FuncBaseDecl,
     FuncDecl,
-    # IfaceDecl,
     Package,
     PackageGroup,
-    # StructDecl,
 )
 from taihe.semantics.types import (
     BOOL,
@@ -40,8 +32,6 @@ from taihe.semantics.visitor import TypeVisitor
 from taihe.utils.analyses import AbstractAnalysis, AnalysisManager
 from taihe.utils.outputs import OutputManager
 
-# from taihe.utils.outputs import OutputBase, OutputManager
-
 
 class KNBridgePackageInfo(AbstractAnalysis[Package]):
     def __init__(self, am: AnalysisManager, p: Package) -> None:
@@ -52,13 +42,23 @@ class KNBridgeFuncBaseDeclInfo(AbstractAnalysis[FuncBaseDecl]):
     def __init__(self, am: AnalysisManager, f: FuncBaseDecl) -> None:
         segments = f.segments
         self.name = encode(segments, DeclKind.FUNCTION)
+        self.konan_proj_name = "_konan_function_0"       # Assuming that information is obtained from IR here
+        self.konan_param_name = "KObjHeader*, KObjHeader*, KObjHeader**"      # Assuming that information is obtained from IR here
+        self.konan_retval_name = "KObjHeader*"     # Assuming that information is obtained from IR here
         info = KNBridgeNormalTypeRefDeclInfo.get(am, f.retvals[0].ty)
         self.return_ty = info.name
         params = []
+        convert_params = []
         for param in f.params:
             abi_param_type_info = KNBridgeParamTypeRefDeclInfo.get(am, param.ty)
             params.append(f"{abi_param_type_info.name} {param.name}")
+            if abi_param_type_info.convert_func:
+                convert_params.append(f"{abi_param_type_info.convert_func}({param.name}, {param.name}_holder.slot())")
+            else:
+                convert_params.append(f"{param.name}")
+        convert_params.append("result_holder.slot()")
         self.params_str = ", ".join(params)
+        self.convert_params_str = ", ".join(convert_params)
 
 
 class KNBridgeNormalTypeRefDeclInfo(AbstractAnalysis[TypeAlike], TypeVisitor):
@@ -68,6 +68,7 @@ class KNBridgeNormalTypeRefDeclInfo(AbstractAnalysis[TypeAlike], TypeVisitor):
         self.name = None
         self.copy_func = None
         self.drop_func = None
+        self.convert_func = None
         self.handle_type(t)
 
     def visit_scalar_type(self, t: ScalarType):
@@ -99,6 +100,7 @@ class KNBridgeParamTypeRefDeclInfo(KNBridgeNormalTypeRefDeclInfo):
     def visit_special_type(self, t: SpecialType) -> Any:
         if t == STRING:
             self.name = "const char*"
+            self.convert_func = "CreateStringFromCString"
         else:
             raise ValueError
 
@@ -118,41 +120,45 @@ class KNBridgeGenerator:
             self.tm, f"include/{kn_bridge_pkg_info.header}", True
         )
 
-        for func in pkg.functions:
-            self.gen_func(func, kn_bridge_pkg_target)
-
-    def gen_func(
-        self,
-        func: FuncDecl,
-        kn_bridge_pkg_target: COutputBuffer,
-    ):
-        kn_bridge_func_info = KNBridgeFuncBaseDeclInfo.get(self.am, func)
-
         kn_bridge_pkg_target.write(
             f"typedef struct {{\n"
             f"  /* User functions. */\n"
             f"  struct {{\n"
             f"    struct {{\n"
-            f"      {kn_bridge_func_info.return_ty} (*{kn_bridge_func_info.name})({kn_bridge_func_info.params_str});\n"
+        )
+        for func in pkg.functions:
+            kn_bridge_func_info = KNBridgeFuncBaseDeclInfo.get(self.am, func)
+            kn_bridge_pkg_target.write(f"      {kn_bridge_func_info.return_ty} (*{kn_bridge_func_info.name})({kn_bridge_func_info.params_str});\n")
+        kn_bridge_pkg_target.write(
             f"    }} root;\n"
             f"  }} kotlin;\n"
             f"}} static_ExportedSymbols;\n"
             f"\n"
-            f'extern "C" void* _konan_function_0();\n'
-            f"static void* _konan_function_0_impl() {{\n"
-            f"  Kotlin_initRuntimeIfNeeded();\n"
-            f"  ScopedRunnableState stateGuard;\n"
-            f"  FrameOverlay* frame = getCurrentFrame();   try {{\n"
-            f"  auto result =   _konan_function_0();\n"
-            f"  return result;\n"
-            f"  }} catch (...) {{      SetCurrentFrame(reinterpret_cast<KObjHeader**>(frame));\n"
-            f"      HandleCurrentExceptionWhenLeavingKotlinCode();\n"
-            f"  }} \n"
-            f"}}\n"
+        )
+        for func in pkg.functions:
+            kn_bridge_func_info = KNBridgeFuncBaseDeclInfo.get(self.am, func)
+            kn_bridge_pkg_target.write(
+                f'extern "C" {kn_bridge_func_info.konan_retval_name} {kn_bridge_func_info.konan_proj_name}({kn_bridge_func_info.konan_param_name});\n'
+                f"static {kn_bridge_func_info.return_ty} {kn_bridge_func_info.konan_proj_name}_impl({kn_bridge_func_info.params_str}) {{\n"
+                f"  Kotlin_initRuntimeIfNeeded();\n"
+                f"  ScopedRunnableState stateGuard;\n"
+                f"  FrameOverlay* frame = getCurrentFrame();   try {{\n"
+                f"  auto result =   {kn_bridge_func_info.konan_proj_name}({kn_bridge_func_info.convert_params_str});\n"
+                f"  return result;\n"
+                f"  }} catch (...) {{      SetCurrentFrame(reinterpret_cast<KObjHeader**>(frame));\n"
+                f"      HandleCurrentExceptionWhenLeavingKotlinCode();\n"
+                f"  }} \n"
+                f"}}\n"
+            )
+        kn_bridge_pkg_target.write(
             f"static static_ExportedSymbols __konan_symbols = {{\n"
             f"  .kotlin = {{\n"
             f"    .root = {{\n"
-            f"      /* {kn_bridge_func_info.name} = */ _konan_function_0_impl,\n"
+        )
+        for func in pkg.functions:
+            kn_bridge_func_info = KNBridgeFuncBaseDeclInfo.get(self.am, func)
+            kn_bridge_pkg_target.write(f"      /* {kn_bridge_func_info.name} = */ {kn_bridge_func_info.konan_proj_name}_impl,\n")
+        kn_bridge_pkg_target.write(
             f"    }},\n"
             f"  }},\n"
             f"}};\n"
