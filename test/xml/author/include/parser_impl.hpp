0e203647fe7fc7279996116a04062fe1ecb048ed
Jemtaly <Jemtaly@outlook.com>
2025-01-04 18:09:21 +0800

    test/xml: improve test code
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- test/xml/author/include/parser_impl.hpp
+++ test/xml/author/include/parser_impl.hpp
@@ -9,17 +9,42 @@
 #include <string>
 #include <vector>
 
+struct ExpatParserState {
+    ohos::xml::ParseOptions const& m_option;
+    std::vector<taihe::core::string> m_stack;
+
+    void onStartElement(const char* name, const char* attrs[]) {
+        m_stack.emplace_back(name);
+        for (const char** iter = attrs; *iter; iter += 2) {
+            if (auto callback = m_option.attributeValueCallbackFunction.get_ptr<ohos::xml::OptCallbackStringStringBool::tag_t::value>()) {
+                (*callback)(iter[0], iter[1]);
+            }
+        }
+    }
+
+    void onEndElement(const char* name) {
+        m_stack.pop_back();
+    }
+
+    void onCharacterData(const char* data, size_t len) {
+        if (m_stack.empty()) {
+            return;
+        }
+        if (auto callback = m_option.tagValueCallbackFunction.get_ptr<ohos::xml::OptCallbackStringStringBool::tag_t::value>()) {
+            (*callback)(m_stack.back(), taihe::core::string_view(data, len));
+        }
+    }
+};
+
 class ExpatParser {
 public:
     ExpatParser(ohos::xml::BufferType const& buffer, ohos::xml::OptString const& encoding)
-        : m_buffer(buffer)
-        , p_option(nullptr) {
+        : m_buffer(buffer) {
         if (auto str = encoding.get_ptr<ohos::xml::OptString::tag_t::value>()) {
             this->m_parser = XML_ParserCreate(str->c_str());
         } else {
             this->m_parser = XML_ParserCreate("UTF-8");
         }
-        XML_SetUserData(m_parser, this);
         XML_SetStartElementHandler(m_parser, StartElementHandler);
         XML_SetEndElementHandler(m_parser, EndElementHandler);
         XML_SetCharacterDataHandler(m_parser, CharacterDataHandler);
@@ -36,56 +61,29 @@ public:
     ExpatParser& operator=(const ExpatParser&&) = delete;
 
     void parse(ohos::xml::ParseOptions const& option) {
-        this->p_option = &option;
+        ExpatParserState state = {option};
+        XML_SetUserData(m_parser, &state);
         XML_Parse(m_parser, m_buffer.content.data(), m_buffer.content.size(), true);
-        this->p_option = nullptr;
-        m_stack.clear();
     }
 
     void parseXml(ohos::xml::ParseOptions const& option) {
-        this->p_option = &option;
+        ExpatParserState state = {option};
+        XML_SetUserData(m_parser, &state);
         XML_Parse(m_parser, m_buffer.content.data(), m_buffer.content.size(), true);
-        this->p_option = nullptr;
-        m_stack.clear();
     }
 
 private:
     static XMLCALL void StartElementHandler(void *userData, const XML_Char *name, const XML_Char **atts) {
-        ((ExpatParser*)userData)->onStartElement(name, atts);
+        ((ExpatParserState*)userData)->onStartElement(name, atts);
     }
     static XMLCALL void EndElementHandler(void *userData, const XML_Char *name) {
-        ((ExpatParser*)userData)->onEndElement(name);
+        ((ExpatParserState*)userData)->onEndElement(name);
     }
     static XMLCALL void CharacterDataHandler(void *userData, const XML_Char *s, int len) {
-        ((ExpatParser*)userData)->onText(s, len);
-    }
-
-private:
-    void onStartElement(const char* name, const char* attrs[]) {
-        m_stack.emplace_back(name);
-        for (const char** iter = attrs; *iter; iter += 2) {
-            if (auto callback = p_option->attributeValueCallbackFunction.get_ptr<ohos::xml::OptCallbackStringStringBool::tag_t::value>()) {
-                (*callback)(iter[0], iter[1]);
-            }
-        }
-    }
-
-    void onEndElement(const char* name) {
-        m_stack.pop_back();
-    }
-
-    void onText(const char* data, size_t len) {
-        if (m_stack.empty()) {
-            return;
-        }
-        if (auto callback = p_option->tagValueCallbackFunction.get_ptr<ohos::xml::OptCallbackStringStringBool::tag_t::value>()) {
-            (*callback)(m_stack.back(), taihe::core::string_view(data, len));
-        }
+        ((ExpatParserState*)userData)->onCharacterData(s, len);
     }
 
 private:
     XML_Parser m_parser;
     ohos::xml::BufferType m_buffer;
-    ohos::xml::ParseOptions const* p_option;
-    std::vector<taihe::core::string> m_stack;
 };
