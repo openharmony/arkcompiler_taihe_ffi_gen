870abe4d0a4f6b3010c5a5e38f1ab9b8a23cb293
Jemtaly <Jemtaly@outlook.com>
2025-02-19 17:11:02 +0800

    codegen: update generated code
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- compiler/taihe/codegen/cpp_impl_generator.py
+++ compiler/taihe/codegen/cpp_impl_generator.py
@@ -4,7 +4,6 @@ from taihe.codegen.abi_generator import (
     TypeABIInfo,
 )
 from taihe.codegen.cpp_proj_generator import (
-    GlobFuncDeclCppProjInfo,
     TypeCppProjInfo,
 )
 from taihe.semantics.declarations import (
@@ -19,6 +18,11 @@ from taihe.utils.outputs import COutputBuffer, OutputManager
 class PackageCppImplInfo(AbstractAnalysis[Package]):
     def __init__(self, am: AnalysisManager, p: Package) -> None:
         self.header = f"{p.name}.impl.hpp"
+
+
+class GlobFuncCppImplInfo(AbstractAnalysis[GlobFuncDecl]):
+    def __init__(self, am: AnalysisManager, f: GlobFuncDecl) -> None:
+        self.macro = f"TH_EXPORT_CPP_API_{f.name}"
 
 
 class CppImplCodeGenerator:
@@ -46,37 +50,31 @@ class CppImplCodeGenerator:
         func: GlobFuncDecl,
         pkg_cpp_impl_target: COutputBuffer,
     ):
-        func_cpp_proj_info = GlobFuncDeclCppProjInfo.get(self.am, func)
         func_abi_info = GlobFuncDeclABIInfo.get(self.am, func)
-        cpp_params = []
+        func_cpp_impl_info = GlobFuncCppImplInfo.get(self.am, func)
+        func_impl = "CPP_FUNC_IMPL"
         args_from_abi = []
         abi_params = []
         for param in func.params:
             type_cpp_proj_info = TypeCppProjInfo.get(self.am, param.ty_ref.resolved_ty)
             type_abi_info = TypeABIInfo.get(self.am, param.ty_ref.resolved_ty)
             pkg_cpp_impl_target.include(*type_cpp_proj_info.defn_headers)
-            cpp_params.append(f"{type_cpp_proj_info.as_param} {param.name}")
             args_from_abi.append(type_cpp_proj_info.pass_from_abi(param.name))
             abi_params.append(f"{type_abi_info.as_param} {param.name}")
-        cpp_params_str = ", ".join(cpp_params)
         args_from_abi_str = ", ".join(args_from_abi)
         abi_params_str = ", ".join(abi_params)
-        cpp_result = f"FUNC_IMPL({args_from_abi_str})"
+        cpp_result = f"{func_impl}({args_from_abi_str})"
         if return_ty_ref := func.return_ty_ref:
             type_cpp_proj_info = TypeCppProjInfo.get(self.am, return_ty_ref.resolved_ty)
             type_abi_info = TypeABIInfo.get(self.am, return_ty_ref.resolved_ty)
             pkg_cpp_impl_target.include(*type_cpp_proj_info.defn_headers)
             abi_return_ty_name = type_abi_info.as_field
-            cpp_return_ty_name = type_cpp_proj_info.as_field
             abi_result = type_cpp_proj_info.return_into_abi(cpp_result)
         else:
             abi_return_ty_name = "void"
-            cpp_return_ty_name = "void"
             abi_result = cpp_result
         pkg_cpp_impl_target.write(
-            f"#define TH_EXPORT_CPP_API_{func.name}(FUNC_IMPL) \\\n"
-            f"    /* TH_STATIC_ASSERT(TH_IS_SAME(TH_TYPEOF(FUNC_IMPL), {cpp_return_ty_name} ({cpp_params_str})), \\\n"
-            f"       \"'\" #FUNC_IMPL \"' is incompatible with '{cpp_return_ty_name} {func_cpp_proj_info.name}({cpp_params_str})'\"); */ \\\n"
+            f"#define {func_cpp_impl_info.macro}({func_impl}) \\\n"
             f"    {abi_return_ty_name} {func_abi_info.mangled_name}({abi_params_str}) {{ \\\n"
             f"        return {abi_result}; \\\n"
             f"    }}\n"
