92e1211279346e72b5c39e971b4a30d1032ed91e
tongdiaoZS <1904807608@qq.com>
2025-03-13 20:29:40 +0800

    test: add impl test
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ test/impl_test/user/src/main.cpp
@@ -0,0 +1,35 @@
+#include "core/object.hpp"
+#include "core/string.hpp"
+#include "impl_test.proj.hpp"
+
+#include <core/callback.hpp>
+
+#include <cstddef>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace impl_test;
+using namespace taihe::core;
+
+#define TRY_CATCH_BLOCK(func) \
+    try { \
+        func; \
+    } catch (const std::runtime_error& e) { \
+        std::cerr << "Caught exception: " << e.what() << std::endl; \
+    } catch (...) { \
+        std::cerr << "Caught unknown exception!" << std::endl; \
+    }
+
+
+int main() {
+    TRY_CATCH_BLOCK(int a = add(1, 2))
+    impl_test::IBase obj = impl_test::makeIBase();
+    TRY_CATCH_BLOCK(obj->getId())
+    impl_test::IBase newobj = obj->getIBase();
+    TRY_CATCH_BLOCK(newobj->getId())
+    TRY_CATCH_BLOCK(newobj->getIBase())
+    return 0;
+}
+
+#undef TRY_CATCH_BLOCK
