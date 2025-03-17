4f8eff69c8277ee95314114d57c50355d707e8e2
tongdiaoZS <1904807608@qq.com>
2025-03-17 20:08:29 +0800

    cookbook: hello_world example
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ cookbook/hello_world/author/src/hello_world.impl.cpp
@@ -0,0 +1,15 @@
+#include "hello_world.impl.hpp"
+#include "stdexcept"
+#include "core/string.hpp"
+// Please delete <stdexcept> include when you implement
+using namespace taihe::core;
+namespace {
+
+string add(int32_t a, int32_t b) {
+    std::string sum = std::to_string(a + b);
+    return sum;
+}
+
+}
+
+TH_EXPORT_CPP_API_add(add)
