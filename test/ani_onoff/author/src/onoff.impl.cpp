a8d92a2a12c5cd0f4bcf438286d0ae01645fc929
tongdiaoZS <1904807608@qq.com>
2025-03-21 21:37:34 +0800

    test/onoff: update onoff example
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- test/ani_onoff/author/src/onoff.impl.cpp
+++ test/ani_onoff/author/src/onoff.impl.cpp
@@ -1,5 +1,6 @@
 #include "onoff.impl.hpp"
 #include "stdexcept"
+#include "core/callback.hpp"
 #include "core/string.hpp"
 #include "onoff.IBase.proj.2.hpp"
 #include <iostream>
@@ -10,59 +11,51 @@ class IBase {
 public:
     IBase(string a, string b): str(a), new_str(b) {}
     ~IBase(){}
-    string onGet() {
-        return str;
+    void onSet(callback_view<void()> a) {
+        a();
+        std::cout << "IBase::onSet" << std::endl;
     }
-    string onGetNew() {
-        return new_str;
-    }
-    void offSet(string_view a) {
-        std::cout << "off set" << std::endl; 
-        str = a;
-    }
-    void offSetNew(string_view a) {
-        std::cout << "off setNew" << std::endl; 
-        new_str = a;
+    void offSet(callback_view<void()> a) {
+        a();
+        std::cout << "IBase::offSet" << std::endl;
     }
 private:
     string str;
     string new_str;
 };
+
 ::onoff::IBase getIBase(string_view a, string_view b) {
     return make_holder<IBase, ::onoff::IBase>(a, b);
 }
-void onFoo(int32_t a) {
-    std::cout << "onFoo" << std::endl; 
+void onFoo(callback_view<void()> a) {
+    a();
+    std::cout << "onFoo" << std::endl;
 }
-void onBar(int32_t a) {
-    std::cout << "onBar" << std::endl; 
+void onBar(callback_view<void()> a) {
+    a();
+    std::cout << "onBar" << std::endl;
 }
-void onBaz(string_view a) {
-    std::cout << "onBaz" << std::endl; 
+void onBaz(int32_t a, callback_view<void()> cb) {
+    cb();
+    std::cout << "a =" << a << ", onBaz" << std::endl;
 }
-void onQux(string_view a, int32_t b) {
-    std::cout << "onQux" << std::endl; 
+void offFoo(callback_view<void()> a) {
+    a();
+    std::cout << "offFoo" << std::endl;
 }
-
-void offFoo(int32_t a) {
-    std::cout << "offFoo" << std::endl; 
+void offBar(callback_view<void()> a) {
+    a();
+    std::cout << "offBar" << std::endl;
 }
-void offBar(int32_t a) {
-    std::cout << "offBar" << std::endl; 
-}
-void offBaz(string_view a) {
-    std::cout << "offBaz" << std::endl; 
-}
-void offQux(string_view a, int32_t b) {
-    std::cout << "offQux" << std::endl; 
+void offBaz(int32_t a, callback_view<void()> cb) {
+    cb();
+    std::cout << "offBaz" << std::endl;
 }
 }
 TH_EXPORT_CPP_API_getIBase(getIBase)
 TH_EXPORT_CPP_API_onFoo(onFoo)
 TH_EXPORT_CPP_API_onBar(onBar)
 TH_EXPORT_CPP_API_onBaz(onBaz)
-TH_EXPORT_CPP_API_onQux(onQux)
 TH_EXPORT_CPP_API_offFoo(offFoo)
 TH_EXPORT_CPP_API_offBar(offBar)
 TH_EXPORT_CPP_API_offBaz(offBaz)
-TH_EXPORT_CPP_API_offQux(offQux)
