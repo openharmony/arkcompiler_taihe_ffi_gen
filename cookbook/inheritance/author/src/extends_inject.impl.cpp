91f85f89019a086cca1613601a1bfffafb1050b6
wangjiaqiang <wangjiaqiang11@huawei.com>
2025-03-21 19:26:26 +0800

    cookbook/inheritance: added
    Signed-off-by: wangjiaqiang <wangjiaqiang11@huawei.com>

---
+++ cookbook/inheritance/author/src/extends_inject.impl.cpp
@@ -0,0 +1,31 @@
+#include "extends_inject.impl.hpp"
+#include "stdexcept"
+#include "extends_inject.InnerPerson.proj.2.hpp"
+#include "core/runtime.hpp"
+// Please delete <stdexcept> include when you implement
+using namespace taihe::core;
+namespace {
+class InnerPerson {
+public:
+    uintptr_t getPseron() {
+        ani_long ani_vtbl_ptr;
+        ani_long ani_data_ptr;
+        ani_class ani_obj_cls;
+        ani_env* env = get_env();
+        env->FindClass("LPerson/Person;", &ani_obj_cls);
+        ani_method ani_obj_ctor;
+        env->Class_FindMethod(ani_obj_cls, "<ctor>", nullptr, &ani_obj_ctor);
+        ani_object ani_obj;
+        env->Object_New(ani_obj_cls, ani_obj_ctor, &ani_obj, ani_vtbl_ptr, ani_data_ptr);
+        return reinterpret_cast<uintptr_t>(ani_obj);
+    }
+
+    ::extends_inject::InnerPerson getInnerPerson() {
+        return make_holder<InnerPerson, ::extends_inject::InnerPerson>();
+    }
+};
+::extends_inject::InnerPerson makePerson() {
+    return make_holder<InnerPerson, ::extends_inject::InnerPerson>();
+}
+}
+TH_EXPORT_CPP_API_makePerson(makePerson)
