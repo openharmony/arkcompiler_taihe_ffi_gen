28b5a40b781ae10371f70154980808a428e7a854
wanzixuan <wanzixuan@huawei.com>
2025-03-24 10:18:43 +0800

    cookbook/union_embed: add map sdk
    Signed-off-by: wanzixuan <wanzixuan@huawei.com>

---
+++ cookbook/union_embed/author/src/hms.core.map.map.impl_inner.cpp
@@ -0,0 +1,66 @@
+#include "core/array.hpp"
+#include "core/runtime.hpp"
+#include "core/string.hpp"
+#include "hms.core.map.map_inner.Resource.proj.2.hpp"
+#include "hms.core.map.map_inner.icons_type.proj.1.hpp"
+#include "hms.core.map.map_inner.impl.hpp"
+#include "hms.core.map.map_inner.map_inner.proj.2.hpp"
+#include "stdexcept"
+// Please delete <stdexcept> include when you implement
+using namespace taihe::core;
+namespace {
+class Resource {
+ public:
+  void foo(int32_t a) {
+    std::cout << "type_Resource: " << "foo: " << a << std::endl;
+  }
+};
+class map_inner {
+ public:
+  string getId() { return "ID got"; }
+  int32_t getZIndex() { return 5; }
+  int32_t getMaxZoom() { return 10; }
+  int32_t getMinZoom() { return 0; }
+  int32_t setIcons_sync(
+      array_view<::hms::core::map::map_inner::icons_type> icons) {
+    for (const auto& element : icons) {
+      switch (element.get_tag()) {
+        case ::hms::core::map::map_inner::icons_type::tag_t::type_string:
+          std::cout << "type_string: " << element.get_type_string_ref()
+                    << std::endl;
+          break;
+        case ::hms::core::map::map_inner::icons_type::tag_t::type_Resource:
+          element.get_type_Resource_ref()->foo(0);
+          break;
+        case ::hms::core::map::map_inner::icons_type::tag_t::type_Image: {
+          auto& obj = element.get_type_Image_ref();
+          ani_class cls;
+          ani_boolean res;
+          ani_env* env = get_env();
+          env->FindClass("Limage/Image_inner;", &cls);
+          env->Object_InstanceOf((ani_object)obj, cls, &res);
+          std::cout << "type_Image: " << (int)res << std::endl;
+          break;
+        }
+      }
+    }
+    return 0;
+  }
+  void setPositions(
+      array_view<array<::hms::core::map::map_inner::Resource>> positions) {
+    for (int i = 0; i < positions.size(); i++) {
+      for (int j = 0; j < positions[i].size(); j++) {
+        positions[i][j]->foo(100);
+      }
+    }
+  }
+};
+::hms::core::map::map_inner::Resource createResource() {
+  return make_holder<Resource, ::hms::core::map::map_inner::Resource>();
+}
+::hms::core::map::map_inner::map_inner make_map_inner() {
+  return make_holder<map_inner, ::hms::core::map::map_inner::map_inner>();
+}
+}  // namespace
+TH_EXPORT_CPP_API_createResource(createResource);
+TH_EXPORT_CPP_API_make_map_inner(make_map_inner);
---
+++ cookbook/union_embed/dts/@hms.core.map.map.d.ts
@@ -0,0 +1,26 @@
+import type image from "@ohos.multimedia.image"
+
+interface BaseOverlay {
+    getId(): string;
+    getZIndex(): number;
+    getTag(): Object;
+    isVisible(): boolean;
+    remove(): void;
+    setZIndex(zIndex: number): void;
+    setTag(tag: Object): void;
+    setVisible(visible: boolean): void;
+}
+
+interface BasePriorityOverlay extends BaseOverlay {
+    getMaxZoom(): number;
+    getMinZoom(): number;
+    setPriority(priority: number): void;
+    setZoom(minZoom: number, maxZoom: number): void;
+    startAnimation(): void;
+    clearAnimation(): void;
+}
+
+interface Bubble extends BasePriorityOverlay {
+    setIcons(icons: Array<string | image.PixelMap | Resource>): Promise<void>;
+    setPositions(positions: Array<Array<mapCommon.LatLng>>): void;
+}
