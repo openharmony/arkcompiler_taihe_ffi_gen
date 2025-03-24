6c82b9eae28936316de2e2726a0819d44411d995
hxf233333 <huxiaofei11@huawei>
2025-03-24 21:52:21 +0800

    cookbook/bigint: added
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ cookbook/bigint/author/src/bigint_test.impl.cpp
@@ -0,0 +1,13 @@
+#include "bigint_test.impl.hpp"
+
+#include "core/array.hpp"
+using namespace taihe::core;
+namespace {
+int64_t getNumfromBigint(array_view<int64_t> a) {
+  int64_t res = a[a.size() - 1] & 0xFF;
+  return res;
+}
+array<int64_t> genBigint(array_view<int64_t> a) { return a; }
+}  // namespace
+TH_EXPORT_CPP_API_getNumfromBigint(getNumfromBigint);
+TH_EXPORT_CPP_API_genBigint(genBigint);
