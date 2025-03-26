f2756b29369c5ce540445e4d711bfa2f495cfb62
whqwe <taylor.wanghao@huawei.com>
2025-03-26 15:11:33 +0800

    test/string: add more tests
    Signed-off-by: whqwe <taylor.wanghao@huawei.com>

--- test/ani_string/author/src/string.op.impl.cpp
+++ test/ani_string/author/src/string.op.impl.cpp
@@ -1,11 +1,31 @@
 #include "core/string.hpp"
 
+#include "core/array.hpp"
 #include "stdexcept"
+#include "string_op.PlayString.proj.2.hpp"
 #include "string_op.StringPair.proj.1.hpp"
 #include "string_op.impl.hpp"
 // Please delete <stdexcept> include when you implement
 using namespace taihe::core;
 namespace {
+class PlayString {
+  string name_{"PlayString"};
+
+ public:
+  string pickString(array_view<string> nums, int32_t n1, int32_t n2) {
+    int32_t size = static_cast<int32_t>(nums.size());
+    if (n1 > n2 || n1 < 0 || n2 >= size) {
+      throw std::runtime_error("index error");
+    }
+    string res{};
+    for (int32_t i = n1; i <= n2; i++) {
+      res = concat(res, nums[i]);
+    }
+    return res;
+  }
+  string getName() { return name_; }
+  void setName(string_view name) { name_ = name; }
+};
 string concatString(string_view a, string_view b) { return concat(a, b); }
 string makeString(string_view a, int32_t b) {
   string result = "";
@@ -30,9 +50,13 @@ string makeString(string_view a, int32_t b) {
 }
 int32_t to_i32(string_view a) { return std::atoi(a.c_str()); }
 string from_i32(int32_t a) { return to_string(a); }
+::string_op::PlayString makePlayStringIface() {
+  return make_holder<PlayString, ::string_op::PlayString>();
+}
 }  // namespace
 TH_EXPORT_CPP_API_concatString(concatString);
 TH_EXPORT_CPP_API_makeString(makeString);
 TH_EXPORT_CPP_API_split(split);
 TH_EXPORT_CPP_API_to_i32(to_i32);
 TH_EXPORT_CPP_API_from_i32(from_i32);
+TH_EXPORT_CPP_API_makePlayStringIface(makePlayStringIface);
