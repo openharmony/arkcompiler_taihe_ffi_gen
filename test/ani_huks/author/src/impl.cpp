af152d2c6fa1e5933c1a471522c23bd3f60bd471
Jemtaly <Jemtaly@outlook.com>
2025-04-23 00:05:26 +0800

    test/huks: make it consistent with the declaration in the SDK
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- test/ani_huks/author/src/impl.cpp
+++ test/ani_huks/author/src/impl.cpp
@@ -1,14 +1,15 @@
-#include <cstdint>
-#include <iostream>
-
 #include "huks.impl.hpp"
+#include "huks.proj.hpp"
 #include "stdexcept"
-#include "taihe/array.hpp"
+#include "taihe/runtime.hpp"
 
 using namespace taihe;
+using namespace huks;
 
 namespace {
-std::string dataAsString(array_view<uint8_t> data) {
+// To be implemented.
+
+std::string ArrayAsString(array_view<uint8_t> data) {
   std::string res;
   int const tempnum = 16;
   for (int i = 0; i < data.size(); i++) {
@@ -19,14 +20,20 @@ std::string dataAsString(array_view<uint8_t> data) {
   }
   return res;
 }
-}  // namespace
 
-namespace {
-::huks::HuksResult generateKey(string_view keyAlias,
-                               ::huks::HuksOptions const &options) {
+std::string BigIntAsString(array<uint64_t> const &data) {
+  std::string res;
+  for (auto const &item : data) {
+    res += std::to_string(item);
+    res += " ";
+  }
+  return res;
+}
+
+HuksResult generateKeySync(string_view keyAlias, HuksOptions const &options) {
   std::cout << "keyAlias: " << keyAlias << std::endl;
   if (auto inData = options.inData) {
-    std::cout << "inData = " << dataAsString(*inData) << std::endl;
+    std::cout << "inData = " << ArrayAsString(*inData) << std::endl;
   } else {
     std::cout << "No inData!" << std::endl;
   }
@@ -36,16 +43,17 @@ namespace {
       std::cout << "tag = " << (size_t)property.tag.get_value() << std::endl;
       switch (property.value.get_tag()) {
       case huks::HuksParamValue::tag_t::bigintValue:
-        std::cout << "bigint " << property.value.get_bigintValue_ref()
+        std::cout << "bigint "
+                  << BigIntAsString(property.value.get_bigintValue_ref())
                   << std::endl;
         break;
       case huks::HuksParamValue::tag_t::booleanValue:
         std::cout << "boolean " << property.value.get_booleanValue_ref()
                   << std::endl;
         break;
-      case huks::HuksParamValue::tag_t::bufferValue:
+      case huks::HuksParamValue::tag_t::arrayValue:
         std::cout << "array "
-                  << dataAsString(property.value.get_bufferValue_ref())
+                  << ArrayAsString(property.value.get_arrayValue_ref())
                   << std::endl;
         break;
       case huks::HuksParamValue::tag_t::numberValue:
@@ -60,7 +68,7 @@ namespace {
   huks::HuksParam huksParam = {
       .tag = huks::HuksTag::key_t::HUKS_TAG_ACCESS_TIME,
       .value =
-          huks::HuksParamValue::make_bufferValue(array<uint8_t>::make(4, 0xcc)),
+          huks::HuksParamValue::make_arrayValue(array<uint8_t>::make(4, 0xcc)),
   };
   huks::HuksResult huksResult = {
       .errorCode = 0.0,
@@ -73,150 +81,170 @@ namespace {
   return huksResult;
 }
 
-void generateKeyItem(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function generateKeyItem Not implemented");
+void generateKeyItemSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "generateKeyItemSync not implemented");
 }
 
-::huks::HuksResult deleteKey(string_view keyAlias,
-                             ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function deleteKey Not implemented");
+HuksResult deleteKeySync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "deleteKeySync not implemented");
 }
 
-void deleteKeyItem(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function deleteKeyItem Not implemented");
+void deleteKeyItemSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "deleteKeyItemSync not implemented");
 }
 
-::huks::HuksResult importKey(string_view keyAlias,
-                             ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function importKey Not implemented");
+HuksResult importKeySync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "importKeySync not implemented");
 }
 
-void importKeyItem(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function importKeyItem Not implemented");
+void importKeyItemSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "importKeyItemSync not implemented");
 }
 
-void importWrappedKeyItem(string_view keyAlias, string_view wrappingKeyAlias,
-                          ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function importWrappedKeyItem Not implemented");
+void importWrappedKeyItemSync(string_view keyAlias,
+                              string_view wrappingKeyAlias,
+                              HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "importWrappedKeyItemSync not implemented");
 }
 
-::huks::HuksResult exportKey(string_view keyAlias,
-                             ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function exportKey Not implemented");
+HuksResult exportKeySync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "exportKeySync not implemented");
 }
 
-::huks::HuksReturnResult exportKeyItem(string_view keyAlias,
-                                       ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function exportKeyItem Not implemented");
+HuksReturnResult exportKeyItemSync(string_view keyAlias,
+                                   HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "exportKeyItemSync not implemented");
 }
 
-::huks::HuksResult getKeyProperties(string_view keyAlias,
-                                    ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function getKeyProperties Not implemented");
+HuksResult getKeyPropertiesSync(string_view keyAlias,
+                                HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "getKeyPropertiesSync not implemented");
 }
 
-::huks::HuksReturnResult getKeyItemProperties(
-    string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function getKeyItemProperties Not implemented");
+HuksReturnResult getKeyItemPropertiesSync(string_view keyAlias,
+                                          HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "getKeyItemPropertiesSync not implemented");
 }
 
-bool isKeyExist(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function isKeyExist Not implemented");
+bool isKeyExistSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "isKeyExistSync not implemented");
 }
 
-bool isKeyItemExist(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function isKeyItemExist Not implemented");
+bool isKeyItemExistSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "isKeyItemExistSync not implemented");
 }
 
-bool hasKeyItem(string_view keyAlias, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function hasKeyItem Not implemented");
+bool hasKeyItemSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "hasKeyItemSync not implemented");
 }
 
-::huks::HuksHandle init(string_view keyAlias,
-                        ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function init Not implemented");
+HuksHandle initSync(string_view keyAlias, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "initSync not implemented");
 }
 
-::huks::HuksSessionHandle initSession(string_view keyAlias,
-                                      ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function initSession Not implemented");
+HuksSessionHandle initSessionSync(string_view keyAlias,
+                                  HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "initSessionSync not implemented");
 }
 
-::huks::HuksResult update(double handle, optional_view<array<uint8_t>> token,
-                          ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function update Not implemented");
+HuksResult updateSync(double handle, optional_view<array<uint8_t>> token,
+                      HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "updateSync not implemented");
 }
 
-::huks::HuksReturnResult updateSession(double handle,
-                                       ::huks::HuksOptions const &options,
-                                       optional_view<array<uint8_t>> token) {
-  throw std::runtime_error("Function updateSession Not implemented");
+HuksReturnResult updateSessionSyncWithoutToken(double handle,
+                                               HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "updateSessionSyncWithoutToken not implemented");
 }
 
-::huks::HuksResult finish(double handle, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function finish Not implemented");
+HuksReturnResult updateSessionSyncWithToken(double handle,
+                                            HuksOptions const &options,
+                                            array_view<uint8_t> token) {
+  TH_THROW(std::runtime_error, "updateSessionSyncWithToken not implemented");
 }
 
-::huks::HuksReturnResult finishSession(double handle,
-                                       ::huks::HuksOptions const &options,
-                                       optional_view<array<uint8_t>> token) {
-  throw std::runtime_error("Function finishSession Not implemented");
+HuksReturnResult updateSessionSync(double handle, HuksOptions const &options,
+                                   optional_view<array<uint8_t>> token) {
+  TH_THROW(std::runtime_error, "updateSessionSync not implemented");
 }
 
-::huks::HuksResult abort(double handle, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function abort Not implemented");
+HuksResult finishSync(double handle, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "finishSync not implemented");
 }
 
-void abortSession(double handle, ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function abortSession Not implemented");
+HuksReturnResult finishSessionSyncWithoutToken(double handle,
+                                               HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "finishSessionSyncWithoutToken not implemented");
 }
 
-::huks::HuksReturnResult attestKeyItem(string_view keyAlias,
-                                       ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function attestKeyItem Not implemented");
+HuksReturnResult finishSessionSyncWithToken(double handle,
+                                            HuksOptions const &options,
+                                            array_view<uint8_t> token) {
+  TH_THROW(std::runtime_error, "finishSessionSyncWithToken not implemented");
 }
 
-::huks::HuksReturnResult anonAttestKeyItem(string_view keyAlias,
-                                           ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function anonAttestKeyItem Not implemented");
+HuksReturnResult finishSessionSync(double handle, HuksOptions const &options,
+                                   optional_view<array<uint8_t>> token) {
+  TH_THROW(std::runtime_error, "finishSessionSync not implemented");
 }
 
-string getSdkVersion(::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function getSdkVersion Not implemented");
+HuksResult abortSync(double handle, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "abortSync not implemented");
 }
 
-::huks::HuksListAliasesReturnResult listAliases(
-    ::huks::HuksOptions const &options) {
-  throw std::runtime_error("Function listAliases Not implemented");
+void abortSessionSync(double handle, HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "abortSessionSync not implemented");
+}
+
+HuksReturnResult attestKeyItemSync(string_view keyAlias,
+                                   HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "attestKeyItemSync not implemented");
+}
+
+HuksReturnResult anonAttestKeyItemSync(string_view keyAlias,
+                                       HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "anonAttestKeyItemSync not implemented");
+}
+
+string getSdkVersion(HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "getSdkVersion not implemented");
+}
+
+HuksListAliasesReturnResult listAliasesSync(HuksOptions const &options) {
+  TH_THROW(std::runtime_error, "listAliasesSync not implemented");
 }
 }  // namespace
 
-// because these macros are auto-generate, lint will cause false positive.
+// Since these macros are auto-generate, lint will cause false positive.
 // NOLINTBEGIN
-TH_EXPORT_CPP_API_generateKey(generateKey);
-TH_EXPORT_CPP_API_generateKeyItem(generateKeyItem);
-TH_EXPORT_CPP_API_deleteKey(deleteKey);
-TH_EXPORT_CPP_API_deleteKeyItem(deleteKeyItem);
-TH_EXPORT_CPP_API_importKey(importKey);
-TH_EXPORT_CPP_API_importKeyItem(importKeyItem);
-TH_EXPORT_CPP_API_importWrappedKeyItem(importWrappedKeyItem);
-TH_EXPORT_CPP_API_exportKey(exportKey);
-TH_EXPORT_CPP_API_exportKeyItem(exportKeyItem);
-TH_EXPORT_CPP_API_getKeyProperties(getKeyProperties);
-TH_EXPORT_CPP_API_getKeyItemProperties(getKeyItemProperties);
-TH_EXPORT_CPP_API_isKeyExist(isKeyExist);
-TH_EXPORT_CPP_API_isKeyItemExist(isKeyItemExist);
-TH_EXPORT_CPP_API_hasKeyItem(hasKeyItem);
-TH_EXPORT_CPP_API_init(init);
-TH_EXPORT_CPP_API_initSession(initSession);
-TH_EXPORT_CPP_API_update(update);
-TH_EXPORT_CPP_API_updateSession(updateSession);
-TH_EXPORT_CPP_API_finish(finish);
-TH_EXPORT_CPP_API_finishSession(finishSession);
-TH_EXPORT_CPP_API_abort(abort);
-TH_EXPORT_CPP_API_abortSession(abortSession);
-TH_EXPORT_CPP_API_attestKeyItem(attestKeyItem);
-TH_EXPORT_CPP_API_anonAttestKeyItem(anonAttestKeyItem);
+TH_EXPORT_CPP_API_generateKeySync(generateKeySync);
+TH_EXPORT_CPP_API_generateKeyItemSync(generateKeyItemSync);
+TH_EXPORT_CPP_API_deleteKeySync(deleteKeySync);
+TH_EXPORT_CPP_API_deleteKeyItemSync(deleteKeyItemSync);
+TH_EXPORT_CPP_API_importKeySync(importKeySync);
+TH_EXPORT_CPP_API_importKeyItemSync(importKeyItemSync);
+TH_EXPORT_CPP_API_importWrappedKeyItemSync(importWrappedKeyItemSync);
+TH_EXPORT_CPP_API_exportKeySync(exportKeySync);
+TH_EXPORT_CPP_API_exportKeyItemSync(exportKeyItemSync);
+TH_EXPORT_CPP_API_getKeyPropertiesSync(getKeyPropertiesSync);
+TH_EXPORT_CPP_API_getKeyItemPropertiesSync(getKeyItemPropertiesSync);
+TH_EXPORT_CPP_API_isKeyExistSync(isKeyExistSync);
+TH_EXPORT_CPP_API_isKeyItemExistSync(isKeyItemExistSync);
+TH_EXPORT_CPP_API_hasKeyItemSync(hasKeyItemSync);
+TH_EXPORT_CPP_API_initSync(initSync);
+TH_EXPORT_CPP_API_initSessionSync(initSessionSync);
+TH_EXPORT_CPP_API_updateSync(updateSync);
+TH_EXPORT_CPP_API_updateSessionSyncWithoutToken(updateSessionSyncWithoutToken);
+TH_EXPORT_CPP_API_updateSessionSyncWithToken(updateSessionSyncWithToken);
+TH_EXPORT_CPP_API_updateSessionSync(updateSessionSync);
+TH_EXPORT_CPP_API_finishSync(finishSync);
+TH_EXPORT_CPP_API_finishSessionSyncWithoutToken(finishSessionSyncWithoutToken);
+TH_EXPORT_CPP_API_finishSessionSyncWithToken(finishSessionSyncWithToken);
+TH_EXPORT_CPP_API_finishSessionSync(finishSessionSync);
+TH_EXPORT_CPP_API_abortSync(abortSync);
+TH_EXPORT_CPP_API_abortSessionSync(abortSessionSync);
+TH_EXPORT_CPP_API_attestKeyItemSync(attestKeyItemSync);
+TH_EXPORT_CPP_API_anonAttestKeyItemSync(anonAttestKeyItemSync);
 TH_EXPORT_CPP_API_getSdkVersion(getSdkVersion);
-TH_EXPORT_CPP_API_listAliases(listAliases);
+TH_EXPORT_CPP_API_listAliasesSync(listAliasesSync);
 // NOLINTEND
