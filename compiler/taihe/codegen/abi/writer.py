a48ed9576b723a917054d5be9ca45c10fa350aab
Mingzhe <asanrocks@outlook.com>
2025-04-29 10:25:49 +0800

    codegen/abi: refactor with C{Header,Source}Writer
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ compiler/taihe/codegen/abi/writer.py
@@ -0,0 +1,32 @@
+from typing import TextIO
+
+from typing_extensions import override
+
+from taihe.utils.outputs import DEFAULT_INDENT, FileWriter, OutputManager
+
+
+class CSourceWriter(FileWriter):
+    """Represents a C or C++ source file."""
+
+    @override
+    def __init__(self, om: OutputManager, path: str, indent_unit: str = DEFAULT_INDENT):
+        super().__init__(om, path=path, indent_unit=indent_unit)
+        self.headers: dict[str, None] = {}
+
+    @override
+    def write_prologue(self, f: TextIO):
+        for header in self.headers:
+            f.write(f'#include "{header}"\n')
+
+    def add_include(self, *headers: str):
+        for header in headers:
+            self.headers.setdefault(header, None)
+
+
+class CHeaderWriter(CSourceWriter):
+    """Represents a C or C++ header file."""
+
+    @override
+    def write_prologue(self, f: TextIO):
+        f.write(f"#pragma once\n")
+        super().write_prologue(f)
