e877c9c3a94212f9f7e571ce961963bb86872cb5
lyujiayi <654765554@qq.com>
2025-06-25 17:34:24 +0800

    test/function&opaque&optional&overload&primitives&record&session&static&string&struct&typedarray&union: modified arktest, float type creation, and asynccallback function types according to frontend changes
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- test/ani_optional/author/src/optional.impl.cpp
+++ test/ani_optional/author/src/optional.impl.cpp
@@ -9,39 +9,39 @@ using namespace taihe;
 namespace {
 class ExampleInterface {
 public:
-  void func_primitive_i8(optional_view<int8_t> param1) {
+  void FuncPrimitiveI8(optional_view<int8_t> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_i16(optional_view<int16_t> param1) {
+  void FuncPrimitiveI16(optional_view<int16_t> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_i32(optional_view<int32_t> param1) {
+  void FuncPrimitiveI32(optional_view<int32_t> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_i64(optional_view<int64_t> param1) {
+  void FuncPrimitiveI64(optional_view<int64_t> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_f32(optional_view<float> param1) {
+  void FuncPrimitiveF32(optional_view<float> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_f64(optional_view<double> param1) {
+  void FuncPrimitiveF64(optional_view<double> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_bool(optional_view<bool> param1) {
+  void FuncPrimitiveBool(optional_view<bool> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_primitive_String(optional_view<string> param1) {
+  void FuncPrimitiveString(optional_view<string> param1) {
     std::cout << "opt1 has value: " << *param1 << std::endl;
   }
 
-  void func_Array(optional_view<array<int32_t>> param1) {
+  void FuncArray(optional_view<array<int32_t>> param1) {
     std::cout << "func_Array: [";
     for (auto const &elem : *param1) {
       std::cout << elem << " ";
@@ -49,7 +49,7 @@ public:
     std::cout << "]" << std::endl;
   }
 
-  void func_Buffer(optional_view<array<uint8_t>> param1) {
+  void FuncBuffer(optional_view<array<uint8_t>> param1) {
     std::cout << "func_Array: [";
     for (auto const &elem : *param1) {
       std::cout << (int)elem << " ";
@@ -57,57 +57,57 @@ public:
     std::cout << "]" << std::endl;
   }
 
-  void func_Union(optional_view<::optional::Union> param1) {
+  void FuncUnion(optional_view<::optional::Union> param1) {
     std::cout << (*param1).get_sValue_ref() << std::endl;
     std::cout << (*param1).get_iValue_ref() << std::endl;
   }
 
-  void func_Map(optional_view<map<string, int32_t>> param1) {
+  void FuncMap(optional_view<map<string, int32_t>> param1) {
     for (auto it = (*param1).begin(); it != (*param1).end(); ++it) {
       auto const &[key, value] = *it;
       std::cout << "Key: " << key << ", Value: " << value << std::endl;
     }
   }
 
-  taihe::optional<string> getName() {
+  taihe::optional<string> GetName() {
     return taihe::optional<string>::make("hello");
   }
 
-  taihe::optional<int8_t> geti8() {
+  taihe::optional<int8_t> Geti8() {
     int8_t const geti8Value = 1;
     return taihe::optional<int8_t>::make(geti8Value);
   }
 
-  taihe::optional<int16_t> geti16() {
+  taihe::optional<int16_t> Geti16() {
     int16_t const geti16Value = 100;
     return taihe::optional<int16_t>::make(geti16Value);
   }
 
-  taihe::optional<int32_t> geti32() {
+  taihe::optional<int32_t> Geti32() {
     int32_t const geti32Value = 1024;
     return taihe::optional<int32_t>::make(geti32Value);  // 默认返回0
   }
 
-  taihe::optional<int64_t> geti64() {
+  taihe::optional<int64_t> Geti64() {
     int64_t const geti64Value = 999999;
     return taihe::optional<int64_t>::make(geti64Value);  // 默认返回0
   }
 
-  taihe::optional<float> getf32() {
+  taihe::optional<float> Getf32() {
     float const getf32Value = 0.0f;
     return taihe::optional<float>::make(getf32Value);
   }
 
-  taihe::optional<double> getf64() {
+  taihe::optional<double> Getf64() {
     double const getf64Value = 0.0;
     return taihe::optional<double>::make(getf64Value);
   }
 
-  taihe::optional<bool> getbool() {
+  taihe::optional<bool> Getbool() {
     return taihe::optional<bool>::make(false);
   }
 
-  taihe::optional<array<uint8_t>> getArraybuffer() {
+  taihe::optional<array<uint8_t>> GetArraybuffer() {
     int const arrSize = 10;
     int const arrNum = 6;
     return taihe::optional<array<uint8_t>>::make(
@@ -115,53 +115,53 @@ public:
   }
 };
 
-::optional::ExampleInterface get_interface() {
+::optional::ExampleInterface GetInterface() {
   return make_holder<ExampleInterface, ::optional::ExampleInterface>();
 }
 
-void printTestInterfaceName(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->getName();
+void PrintTestInterfaceName(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->GetName();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberi8(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->geti8();
+void PrintTestInterfaceNumberi8(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Geti8();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberi16(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->geti16();
+void PrintTestInterfaceNumberi16(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Geti16();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberi32(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->geti16();
+void PrintTestInterfaceNumberi32(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Geti16();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberi64(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->geti64();
+void PrintTestInterfaceNumberi64(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Geti64();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberf32(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->getf32();
+void PrintTestInterfaceNumberf32(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Getf32();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceNumberf64(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->getf64();
+void PrintTestInterfaceNumberf64(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Getf64();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfacebool(::optional::weak::ExampleInterface testiface) {
-  auto res = testiface->getbool();
+void PrintTestInterfacebool(::optional::weak::ExampleInterface testiface) {
+  auto res = testiface->Getbool();
   std::cout << __func__ << ": " << *res << std::endl;
 }
 
-void printTestInterfaceArraybuffer(
+void PrintTestInterfaceArraybuffer(
     ::optional::weak::ExampleInterface testiface) {
-  auto arr = testiface->getArraybuffer();
+  auto arr = testiface->GetArraybuffer();
 
   for (size_t i = 0; i < (*arr).size(); ++i) {
     std::cout << static_cast<int>((*arr).data()[i]);
@@ -174,14 +174,14 @@ void printTestInterfaceArraybuffer(
 
 // because these macros are auto-generate, lint will cause false positive.
 // NOLINTBEGIN
-TH_EXPORT_CPP_API_get_interface(get_interface);
-TH_EXPORT_CPP_API_printTestInterfaceName(printTestInterfaceName);
-TH_EXPORT_CPP_API_printTestInterfaceNumberi8(printTestInterfaceNumberi8);
-TH_EXPORT_CPP_API_printTestInterfaceNumberi16(printTestInterfaceNumberi16);
-TH_EXPORT_CPP_API_printTestInterfaceNumberi32(printTestInterfaceNumberi32);
-TH_EXPORT_CPP_API_printTestInterfaceNumberi64(printTestInterfaceNumberi64);
-TH_EXPORT_CPP_API_printTestInterfaceNumberf32(printTestInterfaceNumberf32);
-TH_EXPORT_CPP_API_printTestInterfaceNumberf64(printTestInterfaceNumberf64);
-TH_EXPORT_CPP_API_printTestInterfacebool(printTestInterfacebool);
-TH_EXPORT_CPP_API_printTestInterfaceArraybuffer(printTestInterfaceArraybuffer);
+TH_EXPORT_CPP_API_GetInterface(GetInterface);
+TH_EXPORT_CPP_API_PrintTestInterfaceName(PrintTestInterfaceName);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberi8(PrintTestInterfaceNumberi8);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberi16(PrintTestInterfaceNumberi16);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberi32(PrintTestInterfaceNumberi32);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberi64(PrintTestInterfaceNumberi64);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberf32(PrintTestInterfaceNumberf32);
+TH_EXPORT_CPP_API_PrintTestInterfaceNumberf64(PrintTestInterfaceNumberf64);
+TH_EXPORT_CPP_API_PrintTestInterfacebool(PrintTestInterfacebool);
+TH_EXPORT_CPP_API_PrintTestInterfaceArraybuffer(PrintTestInterfaceArraybuffer);
 // NOLINTEND
