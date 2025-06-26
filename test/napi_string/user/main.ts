d43fe7a1b35970440e0a519a9d9206b0d049359e
lyujiayi <654765554@qq.com>
2025-06-26 21:57:41 +0800

    test: remove useless files
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ test/napi_string/user/main.ts
@@ -0,0 +1,17 @@
+import { concat, to_i32, from_i32, show } from "../generated/string";
+
+function main() {
+  let concat_str = concat("test", "concat");
+  console.log("function concat: ", concat_str);
+
+  let i32_from_str = to_i32("test");
+  console.log("function to_i32: ", i32_from_str);
+  
+  let str_from_i32 = from_i32(20);
+  console.log("function from_i32: ", str_from_i32);
+  
+  let str_show = show();
+  console.log("function show: ", str_show);
+}
+
+main();
