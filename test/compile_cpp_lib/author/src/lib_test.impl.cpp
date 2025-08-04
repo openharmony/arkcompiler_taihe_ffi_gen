2c160cada159addbe231a0b8c9d102f68fffb543
tongdiaoZS <65269583+tongdiaoZS@users.noreply.github.com>
2025-08-04 16:14:58 +0800

    cmake: CMake refactor (#47)
    * cmake: disassemble the functions of CMakeLists.txt
    
    * cmake: modify CMakeLists.txt to support running a single subproject
    
    * cmake: provides functions add_taihe_ani_library add_taihe_cpp_library
    
    * cmake: TH_ is changed to TAIHE_
    
    * cmake: add optional keyword parameter
    
    * test: add cmake test examples compile_ani_lib, compile_cpp_lib, rgb
    
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

---
+++ test/compile_cpp_lib/author/src/lib_test.impl.cpp
@@ -0,0 +1,41 @@
+#include "lib_test.proj.hpp"
+#include "lib_test.impl.hpp"
+#include "taihe/runtime.hpp"
+#include "stdexcept"
+#include <iostream>
+
+
+namespace {
+// To be implemented.
+
+class IfaceAImpl {
+    public:
+    IfaceAImpl() {}
+
+    void Foo() {
+        std::cout << "IfaceA::Foo" << std::endl;
+    }
+
+    void Bar() {
+        std::cout << "IfaceA::Bar" << std::endl;
+    }
+};
+
+::lib_test::IfaceA MakeIfaceA() {
+    // The parameters in the make_holder function should be of the same type
+    // as the parameters in the constructor of the actual implementation class.
+    return taihe::make_holder<IfaceAImpl, ::lib_test::IfaceA>();
+}
+
+void UseIfaceA(::lib_test::weak::IfaceA obj) {
+    std::cout << "UseIfaceA" << std::endl;
+    obj->Foo();
+    obj->Bar();
+}
+}  // namespace
+
+// Since these macros are auto-generate, lint will cause false positive.
+// NOLINTBEGIN
+TH_EXPORT_CPP_API_MakeIfaceA(MakeIfaceA);
+TH_EXPORT_CPP_API_UseIfaceA(UseIfaceA);
+// NOLINTEND
