82e8a6e6ff278e3fde2b5b2e538189e8b3fe1f06
tongdiaoZS <1904807608@qq.com>
2025-08-18 16:10:19 +0800

    cmake: move the add_ani_demo function
    Signed-off-by: huanghuijin <huanghuijin@huawei.com>

--- cmake/TaiheUtils.cmake
+++ cmake/TaiheUtils.cmake
@@ -505,3 +505,33 @@ function(add_taihe_library target_name idl_files)
   target_include_directories(${target_name} PUBLIC ${GEN_INCLUDE_DIR} ${TAIHE_RUNTIME_HEADER_DIR} ${TAIHE_STDLIB_GEN_INCLUDE_DIR})
   add_dependencies(${target_name} taihe_stdlib_gen)
 endfunction()
+
+function(add_ani_demo demo_name idl_files taihe_configs gen_ets_names user_ets_files user_include_dir user_cpp_files)
+  if (NOT DEFINED TAIHE_STDLIB_DIR)
+    execute_and_set_variable(TAIHE_STDLIB_DIR "--print-stdlib-path")
+  endif()
+
+  set(MAIN_ABC "${CMAKE_CURRENT_BINARY_DIR}/main.abc")
+  
+  # panda sdk 环境配置
+  setup_panda_sdk()
+  # ani 头文件
+  setup_ani_header()
+  # 编译 taihe 运行时
+  add_taihe_runtime()
+  # 编译 taihe 标准库
+  add_taihe_stdlib()
+  # ani 代码生成相关配置
+  set(taihe_configs "-Gpretty-print ${taihe_configs}")
+  # 生成代码
+  generate_code_from_idl(${demo_name} "${idl_files}" "${gen_ets_names}" "cpp-author" "ani-bridge" "${taihe_configs}" GEN_INCLUDE_DIR GEN_ABI_C_FILES GEN_ANI_CPP_FILES GEN_ETS_FILES)
+  # 生成代码静态库编译
+  compile_gen_lib("taihe_gen_${demo_name}" "${GEN_INCLUDE_DIR}" "${GEN_ABI_C_FILES}" "${GEN_ANI_CPP_FILES}")
+  # 动态库编译
+  compile_dylib(${demo_name} "${user_include_dir}" "${user_cpp_files}" "${GEN_INCLUDE_DIR}" "taihe_gen_${demo_name}")
+  # 链接为 main.abc
+  set(ALL_ETS_FILES ${user_ets_files} ${GEN_ETS_FILES})
+  abc_link(${demo_name} ${MAIN_ABC} "${ALL_ETS_FILES}")
+  # 运行
+  run_abc(${demo_name} ${MAIN_ABC})
+endfunction()
