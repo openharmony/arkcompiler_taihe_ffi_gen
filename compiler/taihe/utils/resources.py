c1a0339f8de1efb8318f23f31ac3647b30b26db7
lyujiayi <lyujiayi@huawei.com>
2025-10-11 19:48:59 +0800

    test/ani_finalization: update to adapt new version panda vm
    Signed-off-by: lyujiayi <lyujiayi@huawei.com>

--- compiler/taihe/utils/resources.py
+++ compiler/taihe/utils/resources.py
@@ -369,8 +369,8 @@ class _LegacyPandaVm(PathResource):
 class PandaVm(CachedResource):
     CLI_NAME = "panda-vm"
     PATH_CACHE = "panda-vm"
-    VERSION: Final = "sdk-1.5.0-dev.47414"
-    URL: Final = "https://gitcode.com/m0_52007851/panda_vm/releases/download/47414"
+    VERSION: Final = "sdk-1.5.0-dev.49226"
+    URL: Final = "https://gitcode.com/m0_52007851/panda_vm/releases/download/49226"
 
     # Computed attributes
     ani_header_dir: Path = field(init=False)
