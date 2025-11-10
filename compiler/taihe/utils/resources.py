0914da2a6316120720af74449e72370721cb8093
lyujiayi <lyujiayi@huawei.com>
2025-11-10 20:55:42 +0800

    scripts/build: update python version
    Signed-off-by: lyujiayi <lyujiayi@huawei.com>

--- compiler/taihe/utils/resources.py
+++ compiler/taihe/utils/resources.py
@@ -467,7 +467,7 @@ class PythonBuild(CachedResource):
     DARWIN_X86_64: Final = "darwin-x86_64"
 
     # Bundle tarball file names for each platform
-    LINUX_X86_64_PY_BUNDLE: Final = "python-linux-x86-GLIBC2.35-3.11.4_20251107.tar.gz"
+    LINUX_X86_64_PY_BUNDLE: Final = "python-linux-x86-GLIBC2.27-3.11.4_20251107.tar.gz"
     WINDOWS_X86_64_PY_BUNDLE: Final = "python-mingw-x86-3.11.4_20251107.tar.gz"
     DARWIN_ARM64_PY_BUNDLE: Final = "python-darwin-arm64-3.11.4_20251107.tar.gz"
     DARWIN_X86_64_PY_BUNDLE: Final = "python-darwin-x86-3.11.4_20251107.tar.gz"
