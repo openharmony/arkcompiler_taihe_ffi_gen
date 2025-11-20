00180e9a5e4ac1ae402e4746857cb9a1fcd9422c
Jemtaly <Jemtaly@outlook.com>
2025-11-20 15:51:43 +0800

    test: add ani_class_extends
    Signed-off-by: wanzixuan <wanzixuan@huawei.com>

---
+++ test/ani_class_extends/author/src/foo.impl.cpp
@@ -0,0 +1,24 @@
+#include "foo.impl.hpp"
+#include "foo.DerivedMethodClass.template.hpp"
+#include "stdexcept"
+
+namespace {
+::foo::DerivedMethodClass makeDerivedMethodClass() {
+  // The parameters in the make_holder function should be of the same type
+  // as the parameters in the constructor of the actual implementation class.
+  return taihe::make_holder<DerivedMethodClassImpl,
+                            ::foo::DerivedMethodClass>();
+}
+
+::foo::DerivedDataClass makeDerivedDataClass() {
+  return {
+    .base = {"base"}, .foo = {"foo"}, .bar = {"bar"}, .x = 42, .y = 56,
+  };
+}
+}  // namespace
+
+// Since these macros are auto-generate, lint will cause false positive.
+// NOLINTBEGIN
+TH_EXPORT_CPP_API_makeDerivedMethodClass(makeDerivedMethodClass);
+TH_EXPORT_CPP_API_makeDerivedDataClass(makeDerivedDataClass);
+// NOLINTEND
