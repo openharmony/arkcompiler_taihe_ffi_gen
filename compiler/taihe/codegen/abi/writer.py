7d6600a0b6e6792d4ee2b440ab170433865494d3
Jemtaly <Jemtaly@outlook.com>
2025-12-19 13:56:38 +0800

    utils/outputs: remove DEFAULT_INDENT
    Signed-off-by: wanzixuan <wanzixuan@huawei.com>

--- compiler/taihe/codegen/abi/writer.py
+++ compiler/taihe/codegen/abi/writer.py
@@ -2,7 +2,7 @@ from typing import TextIO
 
 from typing_extensions import override
 
-from taihe.utils.outputs import DEFAULT_INDENT, FileKind, FileWriter, OutputManager
+from taihe.utils.outputs import FileKind, FileWriter, OutputManager
 
 
 class CSourceWriter(FileWriter):
@@ -13,13 +13,12 @@ class CSourceWriter(FileWriter):
         om: OutputManager,
         relative_path: str,
         file_kind: FileKind,
-        indent_unit: str = DEFAULT_INDENT,
     ):
         super().__init__(
             om,
             relative_path=relative_path,
             file_kind=file_kind,
-            default_indent=indent_unit,
+            default_indent="    ",
             comment_prefix="// ",
         )
         self.headers: dict[str, None] = {}
