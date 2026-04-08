263421c73c8617b20d6c83d8041a6359bbf5e94f
lyujiayi <lyujiayi@huawei.com>
2026-04-08 11:45:26 +0800

    codegen/abi: update options to register NoexceptAllOption
    Signed-off-by: lyujiayi <lyujiayi@huawei.com>
    Co-Authored-By: NA

--- compiler/taihe/codegen/abi/__init__.py
+++ compiler/taihe/codegen/abi/__init__.py
@@ -52,17 +52,75 @@ class AbiHeaderBackendConfig(BackendConfig):
 class AbiSourcesBackendConfig(BackendConfig):
     NAME = "abi-source"
     DEPS: ClassVar = ["abi-header"]
+    
+    noexcept_all: bool = False
+
+    @classmethod
+    def register(cls, option_registry: "OptionRegistry"):
+        from taihe.codegen.abi.options import all_abi_config_options
+
+        option_registry.register(*all_abi_config_options)
 
     @classmethod
     def create(cls, options: "OptionStore", dm: "DiagnosticsManager"):
-        return AbiSourcesBackendConfig()
+        from taihe.codegen.abi.options import NoexceptAllOption
+
+        noexcept_all_opt = options.get(NoexceptAllOption)
+        return AbiSourcesBackendConfig(
+            noexcept_all=noexcept_all_opt is not None,
+        )
 
     def construct(self, instance: "CompilerInstance"):
         from taihe.codegen.abi.gen_abi import AbiSourcesGenerator
+        from taihe.codegen.abi.attributes import NoexceptAttr
+        from taihe.semantics.declarations import CallbackTypeRefDecl
 
         class AbiSourcesBackendImpl(Backend):
-            def __init__(self, ci: "CompilerInstance"):
+            def __init__(self, ci: "CompilerInstance", config: AbiSourcesBackendConfig):
                 self._ci = ci
+                self._config = config
+
+            def post_process(self):
+                if self._config.noexcept_all:
+                    self._add_noexcept_all()
+
+            def _add_noexcept_all(self):
+                """Helper method to add @noexcept to all applicable declarations."""
+                from taihe.semantics.declarations import (
+                    GlobFuncDecl,
+                    IfaceMethodDecl,
+                )
+                from taihe.semantics.visitor import RecursiveDeclVisitor
+
+                class NoexceptCallbackVisitor(RecursiveDeclVisitor):
+                    """Visitor that adds @noexcept to all callback types."""
+
+                    def __init__(self):
+                        super().__init__()
+
+                    def visit_glob_func(self, d: GlobFuncDecl) -> None:
+                        """Visit global function and add @noexcept if needed."""
+                        if NoexceptAttr.get(d) is None:
+                            d.add_attribute(NoexceptAttr(loc=d.loc))
+                        super().visit_glob_func(d)
+
+                    def visit_iface_method(self, d: IfaceMethodDecl) -> None:
+                        """Visit interface method and add @noexcept if needed."""
+                        if NoexceptAttr.get(d) is None:
+                            d.add_attribute(NoexceptAttr(loc=d.loc))
+                        super().visit_iface_method(d)
+
+                    def visit_callback_type_ref(self, d: CallbackTypeRefDecl) -> None:
+                        """Visit callback type reference and add @noexcept."""
+                        if NoexceptAttr.get(d) is None:
+                            d.add_attribute(NoexceptAttr(loc=d.loc))
+                        # Recurse into parameters and return type
+                        super().visit_callback_type_ref(d)
+
+                # Apply visitor to all packages
+                visitor = NoexceptCallbackVisitor()
+                for p in self._ci.package_group.iterate():
+                    p.accept(visitor)
 
             def generate(self):
                 self._ci.output_manager.register_runtime_cxx_src("string.cpp")
@@ -72,7 +130,7 @@ class AbiSourcesBackendConfig(BackendConfig):
                 pg = self._ci.package_group
                 AbiSourcesGenerator(om, am).generate(pg)
 
-        return AbiSourcesBackendImpl(instance)
+        return AbiSourcesBackendImpl(instance, self)
 
 
 @dataclass
