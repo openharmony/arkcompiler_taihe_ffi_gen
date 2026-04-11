908e953f3b1cf7480bd4374fa755045a2aeaf85a
lyujiayi <lyujiayi@huawei.com>
2026-04-11 15:44:56 +0800

    test/napi: add ra_napi_image, napi_primitives, napi_bundlemanager and napi_bundle
    Signed-off-by: lyujiayi <lyujiayi@huawei.com>
    Co-Authored-By: Agent

---
+++ test/napi_bundlemanager/author/src/skill.napi_register.cpp
@@ -0,0 +1,27 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+
+#include "skill.napi.h"
+
+napi_value Init(napi_env env, napi_value exports)
+{
+    skill::NapiInit(env, exports);
+    return exports;
+}
+
+static napi_module demoModule = {
+    .nm_version = 1,
+    .nm_flags = 0,
+    .nm_filename = nullptr,
+    .nm_register_func = Init,
+    .nm_modname = "entry",
+    .nm_priv = ((void *)0),
+    .reserved = {0},
+};
+
+extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
+{
+    napi_module_register(&demoModule);
+}
+
+#pragma clang diagnostic pop
