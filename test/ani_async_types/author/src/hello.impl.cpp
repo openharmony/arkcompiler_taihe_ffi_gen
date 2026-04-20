a1e84eac62872c53e801599c03332da42c7eeecd
Jemtaly <Jemtaly@outlook.com>
2026-04-20 22:09:00 +0800

    test: Adds async and sync error handling to async type tests
    Signed-off-by: Jemtaly <Jemtaly@outlook.com>

--- test/ani_async_types/author/src/hello.impl.cpp
+++ test/ani_async_types/author/src/hello.impl.cpp
@@ -15,7 +15,6 @@
 
 #include "hello.impl.hpp"
 #include "hello.proj.hpp"
-#include "stdexcept"
 
 #include <chrono>
 #include <iostream>
@@ -24,33 +23,56 @@
 namespace {
 using expected_type = ::taihe::expected<::taihe::string, ::taihe::error>;
 
-::taihe::expected<void, ::taihe::error> futureResultWithCallback(int64_t ms, ::taihe::string_view val,
+::taihe::expected<void, ::taihe::error> FutureResultWithCallback(int64_t ms, ::taihe::string_view val,
                                                                  ::taihe::completer<expected_type> completer)
 {
+    if (ms < 0) {
+        return taihe::unexpected<taihe::error>("ms cannot be negative");
+    }
     std::thread([ms, val = taihe::string(val), completer = std::move(completer)]() mutable {
         std::cout << "[Future Result] Waiting for " << ms << " milliseconds..." << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(ms));
         std::cout << "[Future Result] Task completed, completing future..." << std::endl;
-        completer.complete("C++AsyncProcessed-" + val);
+        if (val.empty()) {
+            completer.complete(taihe::unexpected<taihe::error>("val cannot be empty"));
+        } else {
+            completer.complete("C++AsyncProcessed-" + val);
+        }
     }).detach();
     return {};
 }
 
-taihe::future<expected_type> futureResultReturnsPromise(int64_t ms, ::taihe::string_view val)
+::taihe::expected<::taihe::future<expected_type>, ::taihe::error> FutureResultReturnsPromise(int64_t ms,
+                                                                                             ::taihe::string_view val)
 {
     auto [completer, future] = taihe::make_async_pair<expected_type>();
-    futureResultWithCallback(ms, val, std::move(completer));
+    if (ms < 0) {
+        return taihe::unexpected<taihe::error>("ms cannot be negative");
+    }
+    std::thread([ms, val = taihe::string(val), completer = std::move(completer)]() mutable {
+        std::cout << "[Future Result Returns Promise] Waiting for " << ms << " milliseconds..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+        std::cout << "[Future Result Returns Promise] Task completed, completing future..." << std::endl;
+        if (val.empty()) {
+            completer.complete(taihe::unexpected<taihe::error>("val cannot be empty"));
+        } else {
+            completer.complete("C++AsyncProcessed-" + val);
+        }
+    }).detach();
     return std::move(future);
 }
 
-void processUserTypeWithCallback(::hello::weak::UserType user, int64_t ets_ms, int64_t cpp_ms,
-                                 ::taihe::string_view result, ::taihe::completer<expected_type> fin)
+::taihe::expected<void, ::taihe::error> ProcessUserTypeWithCallback(::hello::weak::UserType user, int64_t etsMs,
+                                                                    int64_t cppMs, ::taihe::string_view result,
+                                                                    ::taihe::completer<expected_type> fin)
 {
-    std::cout << "[Process UserType With Callback] Calling UserType method with callback..." << std::endl;
+    if (etsMs < 0 || cppMs < 0) {
+        return taihe::unexpected<taihe::error>("ms cannot be negative");
+    }
 
     auto [mid, tmp] = taihe::make_async_pair<expected_type>();
 
-    auto exp = user->fooWithCallback(ets_ms, "C++SyncProcessed-" + result, std::move(mid));
+    auto exp = user->fooWithCallback(etsMs, "C++SyncProcessed-" + result, std::move(mid));
 
     if (not exp) {
         std::cerr << "[Process UserType With Callback] Error calling UserType method: " << exp.error().message()
@@ -60,10 +82,10 @@ void processUserTypeWithCallback(::hello::weak::UserType user, int64_t ets_ms, i
         std::cerr << "[Process UserType With Callback] UserType method called successfully, waiting for result..."
                   << std::endl;
 
-        tmp.on_complete([fin = std::move(fin), cpp_ms](expected_type &&res) mutable {
+        tmp.on_complete([fin = std::move(fin), cppMs](expected_type &&res) mutable {
             if (res) {
                 std::cout << "[Process UserType With Callback] UserType method completed successfully." << std::endl;
-                std::this_thread::sleep_for(std::chrono::milliseconds(cpp_ms));
+                std::this_thread::sleep_for(std::chrono::milliseconds(cppMs));
                 fin.complete("C++AsyncProcessed-" + res.value());
             } else {
                 std::cerr << "[Process UserType With Callback] UserType method completed with error: "
@@ -72,14 +94,18 @@ void processUserTypeWithCallback(::hello::weak::UserType user, int64_t ets_ms, i
             }
         });
     }
+
+    return {};
 }
 
-taihe::future<expected_type> processUserTypeReturnsPromise(::hello::weak::UserType user, int64_t ets_ms, int64_t cpp_ms,
-                                                           ::taihe::string_view result)
+::taihe::expected<::taihe::future<expected_type>, ::taihe::error> ProcessUserTypeReturnsPromise(
+    ::hello::weak::UserType user, int64_t etsMs, int64_t cppMs, ::taihe::string_view result)
 {
-    std::cout << "[Process UserType Returns Promise] Calling UserType method that returns promise..." << std::endl;
+    if (etsMs < 0 || cppMs < 0) {
+        return taihe::unexpected<taihe::error>("ms cannot be negative");
+    }
 
-    auto exp = user->fooReturnsPromise(ets_ms, "C++SyncProcessed-" + result);
+    auto exp = user->fooReturnsPromise(etsMs, "C++SyncProcessed-" + result);
 
     auto [fin, fut] = taihe::make_async_pair<expected_type>();
 
@@ -92,10 +118,10 @@ taihe::future<expected_type> processUserTypeReturnsPromise(::hello::weak::UserTy
                      "complete..."
                   << std::endl;
 
-        exp.value().on_complete([fin = std::move(fin), cpp_ms](expected_type &&res) mutable {
+        exp.value().on_complete([fin = std::move(fin), cppMs](expected_type &&res) mutable {
             if (res) {
                 std::cout << "[Process UserType Returns Promise] UserType method completed successfully." << std::endl;
-                std::this_thread::sleep_for(std::chrono::milliseconds(cpp_ms));
+                std::this_thread::sleep_for(std::chrono::milliseconds(cppMs));
                 fin.complete("C++AsyncProcessed-" + res.value());
             } else {
                 std::cerr << "[Process UserType Returns Promise] UserType method completed with error: "
@@ -111,8 +137,8 @@ taihe::future<expected_type> processUserTypeReturnsPromise(::hello::weak::UserTy
 
 // Since these macros are auto-generate, lint will cause false positive.
 // NOLINTBEGIN
-TH_EXPORT_CPP_API_futureResultWithCallback(futureResultWithCallback);
-TH_EXPORT_CPP_API_futureResultReturnsPromise(futureResultReturnsPromise);
-TH_EXPORT_CPP_API_processUserTypeWithCallback(processUserTypeWithCallback);
-TH_EXPORT_CPP_API_processUserTypeReturnsPromise(processUserTypeReturnsPromise);
+TH_EXPORT_CPP_API_futureResultWithCallback(FutureResultWithCallback);
+TH_EXPORT_CPP_API_futureResultReturnsPromise(FutureResultReturnsPromise);
+TH_EXPORT_CPP_API_processUserTypeWithCallback(ProcessUserTypeWithCallback);
+TH_EXPORT_CPP_API_processUserTypeReturnsPromise(ProcessUserTypeReturnsPromise);
 // NOLINTEND
