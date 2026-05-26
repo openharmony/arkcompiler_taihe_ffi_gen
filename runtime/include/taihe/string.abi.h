b4c2bfaf44cab909900a37464c16773ab486739d
tongdiaoZS <1904807608@qq.com>
2026-05-26 10:57:53 +0800

    runtime/string: taihe string refactor
    Signed-off-by: tongdiaoZS <1904807608@qq.com>

--- runtime/include/taihe/string.abi.h
+++ runtime/include/taihe/string.abi.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -26,18 +26,25 @@
 /////////////////////////////////////////
 
 enum TStringFlags {
-    TSTRING_REF = 1,
+    TSTRING_MODE_MASK = 0xFFFF,
+    TSTRING_ENCODING_MASK = 0xFFFF0000,
+    TSTRING_REF = 1u,
+    TSTRING_EXT = 1u << 1,
+    TSTRING_UTF8 = 1u << 16,
+    TSTRING_UTF16 = 1u << 17,
+};
+
+struct TStringInfo {
+    TRefCount count;
+    void (*drop)(void *);
+    void *external_obj;
 };
 
 struct TString {
     uint32_t flags;
     uint32_t length;
-    char const *ptr;  // always valid and non-null
-};
-
-struct TStringData {
-    TRefCount count;
-    char buffer[];
+    struct TStringInfo *pstrinfo;
+    char const *ptr;
 };
 
 //////////////////
@@ -56,7 +63,8 @@ TH_INLINE size_t tstr_len(struct TString tstr)
     return tstr.length;
 }
 
-// Allocates memory and initializes a TString with a given capacity.
+// Allocates memory and initializes a UTF8 encoding TString with a given
+// capacity.
 //
 // # Arguments
 // - `tstr_ptr`: Pointer to an uninitialized TString structure.
@@ -70,7 +78,22 @@ TH_INLINE size_t tstr_len(struct TString tstr)
 // - Reference count is set to 1 after called.
 TH_EXPORT char *tstr_initialize(struct TString *tstr_ptr, uint32_t capacity);
 
-// Creates a new heap-allocated TString by copying an existing string.
+// Allocates memory and initializes a UTF16 encoding TString with a given
+// capacity.
+//
+// # Arguments
+// - `tstr_ptr`: Pointer to an uninitialized TString structure.
+// - `capacity`: The desired capacity of the string buffer.
+//
+// # Returns
+// - Pointer to the allocated buffer.
+//
+// # Notes
+// - The caller is responsible for setting the string length.
+// - Reference count is set to 1 after called.
+TH_EXPORT uint16_t *tstr_initialize_utf16(struct TString *tstr_ptr, uint32_t capacity);
+
+// Creates a new heap-allocated TString by copying an existing UTF8 string.
 //
 // # Arguments
 // - `buf`: A null-terminated string (must not be null).
@@ -83,7 +106,7 @@ TH_EXPORT char *tstr_initialize(struct TString *tstr_ptr, uint32_t capacity);
 // - The returned TString must be freed using `tstr_drop`.
 TH_EXPORT struct TString tstr_new(char const *buf TH_NONNULL, size_t len);
 
-// Creates a TString from an existing string.
+// Creates a TString from an existing UTF8 string.
 //
 // # Arguments
 // - `buf`: a null-terminated string. Null pointer is invalid.
@@ -99,6 +122,36 @@ TH_EXPORT struct TString tstr_new(char const *buf TH_NONNULL, size_t len);
 //    In this case, the original `tstr` is still uninitialized and should not be
 //    used.
 TH_EXPORT struct TString tstr_new_ref(char const *buf TH_NONNULL, size_t len);
+
+// Creates a new heap-allocated TString by copying an existing UTF16 string.
+//
+// # Arguments
+// - `buf`: A null-terminated string (must not be null).
+// - `len`: The length of the string.
+//
+// # Returns
+// - A new TString containing a copy of `buf`.
+//
+// # Notes
+// - The returned TString must be freed using `tstr_drop`.
+TH_EXPORT struct TString tstr_new_utf16(uint16_t const *buf TH_NONNULL, size_t len);
+
+// Creates a TString from an existing UTF16 string.
+//
+// # Arguments
+// - `buf`: a null-terminated string. Null pointer is invalid.
+// - `len`: the length of the string.
+// - `tstr`: pointer to an uninitialized TString. Do not pass an
+//    already-initialized TString here.
+//
+// # Returns
+// - `tstr`, if the string is created successfully. The caller must ensure the
+//    string buffer and the returned TString remain unchanged during the whole
+//    lifetime of the TString.
+// - `NULL`, if the string is not null-terminated, or the length is too large.
+//    In this case, the original `tstr` is still uninitialized and should not be
+//    used.
+TH_EXPORT struct TString tstr_new_ref_utf16(uint16_t const *buf TH_NONNULL, size_t len);
 
 // Frees a TString, releasing allocated memory if applicable.
 //
@@ -123,7 +176,7 @@ TH_EXPORT void tstr_drop(struct TString tstr);
 // - Use `tstr_drop` to free the duplicate when done.
 TH_EXPORT struct TString tstr_dup(struct TString tstr);
 
-// Concatenates two TString objects.
+// Concatenates two UTF-8 TString objects.
 //
 // # Parameters
 // - `count`: The number of strings to concatenate.
@@ -136,7 +189,7 @@ TH_EXPORT struct TString tstr_dup(struct TString tstr);
 // - The returned TString must be freed using `tstr_drop`.
 TH_EXPORT struct TString tstr_concat(size_t count, struct TString const *tstr_list);
 
-// Extracts a substring from a TString object.
+// Extracts a substring from a UTF-8 TString object.
 //
 // # Parameters
 // - `tstr`: The source TString object to extract the substring from.
@@ -151,5 +204,80 @@ TH_EXPORT struct TString tstr_concat(size_t count, struct TString const *tstr_li
 // - The returned TString is just a view of the original string and does not own
 //   the memory, so it should not be freed.
 TH_EXPORT struct TString tstr_substr(struct TString tstr, size_t pos, size_t len);
+
+// Retrieves the encoding type of a TString.
+//
+// # Parameters
+// - `tstr`: The TString whose encoding type is to be retrieved.
+//
+// # Returns
+// - A `uint32_t` value representing the encoding of the TString.
+//   user can compare with TSTRING_UTF8 / TSTRING_UTF16
+//   Possible values typically correspond to UTF-8, UTF-16, or UTF-32.
+//
+// # Notes
+// - This function does not modify the TString.
+TH_EXPORT uint32_t tstr_encoding(struct TString tstr);
+
+// Converts a UTF8-encoded TString object into a UTF16-encoded TString.
+//
+// # Parameters
+// - `utf8_str`: The source TString encoded in UTF8.
+//
+// # Returns
+// - A new TString encoded in UTF16.
+//   The returned TString owns its memory and must be freed with `tstr_drop`.
+//
+// # Notes
+// - Invalid UTF8 sequences are handled according to the internal conversion
+//   policy (typically replacing invalid sequences with U+FFFD).
+// - Serious errors return an empty string U'\0'.
+// - The returned TString is heap-allocated and independent of the input.
+TH_EXPORT struct TString tstr_utf8_to_utf16(struct TString utf8_str);
+
+// Converts a UTF16-encoded TString object into a UTF8-encoded TString.
+//
+// # Parameters
+// - `utf16_str`: The source TString encoded in UTF16.
+//
+// # Returns
+// - A new TString encoded in UTF8.
+//   The returned TString owns its memory and must be freed with `tstr_drop`.
+//
+// # Notes
+// - Invalid surrogate pairs or malformed UTF16 sequences are handled according
+//   to the internal conversion policy (typically replacing invalid sequences
+//   with U+FFFD).
+// - The returned TString is heap-allocated and independent of the input.
+TH_EXPORT struct TString tstr_utf16_to_utf8(struct TString utf16_str);
+
+// Concatenates two UTF-16 TString objects.
+//
+// # Parameters
+// - `count`: The number of strings to concatenate.
+// - `tstr_list`: An array of TString objects to concatenate.
+//
+// # Returns
+// - A new TString object containing the concatenated result.
+//
+// # Notes
+// - The returned TString must be freed using `tstr_drop`.
+TH_EXPORT struct TString tstr_concat_utf16(size_t count, struct TString const *tstr_list);
+
+// Extracts a substring from a UTF-16 TString object.
+//
+// # Parameters
+// - `tstr`: The source TString object to extract the substring from.
+// - `pos`: The starting position of the substring within the source TString
+//   object.
+// - `len`: The length of the substring to extract.
+//
+// # Returns
+// - A TString reference of the extracted substring.
+//
+// # Notes
+// - The returned TString is just a view of the original string and does not own
+//   the memory, so it should not be freed.
+TH_EXPORT struct TString tstr_substr_utf16(struct TString tstr, size_t pos, size_t len);
 
 #endif  // TAIHE_STRING_ABI_H
--- runtime/src/string.cpp
+++ runtime/src/string.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,31 +17,48 @@
 
 #include <algorithm>
 
-// Converts a TString into its corresponding heap-allocated TStringData
-// structure.
-//
-// # Arguments
-// - `tstr`: The TString to be converted.
-//
-// # Returns
-// - A pointer to the TStringData structure if the TString is heap-allocated.
-// - `nullptr` if the TString is a reference (TSTRING_REF is set).
-TH_INLINE struct TStringData *to_heap(struct TString tstr)
+uint32_t tstr_encoding(struct TString tstr)
 {
-    if (tstr.flags & TSTRING_REF) {
-        return nullptr;
-    }
-    return reinterpret_cast<struct TStringData *>(const_cast<char *>(tstr.ptr) - offsetof(struct TStringData, buffer));
+    return (tstr.flags & TSTRING_ENCODING_MASK);
 }
 
 char *tstr_initialize(struct TString *tstr_ptr, uint32_t capacity)
 {
-    size_t bytes_required = sizeof(struct TStringData) + sizeof(char) * capacity;
-    struct TStringData *sh = reinterpret_cast<struct TStringData *>(malloc(bytes_required));
+    size_t char_size = sizeof(char);
+    size_t bytes_required = sizeof(struct TStringInfo) + capacity * char_size;
+    struct TStringInfo *sh = reinterpret_cast<struct TStringInfo *>(malloc(bytes_required));
+    if (!sh) return nullptr;
+
     tref_init(&sh->count, 1);
-    tstr_ptr->flags = 0;
-    tstr_ptr->ptr = sh->buffer;
-    return sh->buffer;
+    sh->drop = nullptr;
+    sh->external_obj = nullptr;
+
+    char *buffer = reinterpret_cast<char *>(sh + 1);
+
+    tstr_ptr->flags = TSTRING_UTF8;
+    tstr_ptr->pstrinfo = sh;
+    tstr_ptr->ptr = buffer;
+
+    return buffer;
+}
+
+uint16_t *tstr_initialize_utf16(struct TString *tstr_ptr, uint32_t capacity)
+{
+    size_t char_size = sizeof(uint16_t);
+    size_t bytes_required = sizeof(struct TStringInfo) + char_size * capacity;
+    struct TStringInfo *sh = reinterpret_cast<struct TStringInfo *>(malloc(bytes_required));
+    if (!sh) return nullptr;
+
+    tref_init(&sh->count, 1);
+    sh->drop = nullptr;
+    sh->external_obj = nullptr;
+
+    char *buffer = reinterpret_cast<char *>(sh + 1);
+
+    tstr_ptr->flags = TSTRING_UTF16;
+    tstr_ptr->pstrinfo = sh;
+    tstr_ptr->ptr = buffer;
+    return reinterpret_cast<uint16_t *>(buffer);
 }
 
 struct TString tstr_new(char const *value TH_NONNULL, size_t len)
@@ -54,34 +71,419 @@ struct TString tstr_new(char const *value TH_NONNULL, size_t len)
     return tstr;
 }
 
+struct TString tstr_new_utf16(uint16_t const *value TH_NONNULL, size_t len)
+{
+    struct TString tstr;
+    uint16_t *buf = tstr_initialize_utf16(&tstr, len + 1);
+    std::copy(value, value + len, buf);
+    buf[len] = u'\0';
+    tstr.length = len * sizeof(uint16_t);
+    return tstr;
+}
+
 struct TString tstr_new_ref(char const *buf TH_NONNULL, size_t len)
 {
     struct TString tstr;
-    tstr.flags = TSTRING_REF;
+    tstr.flags = TSTRING_REF | TSTRING_UTF8;
     tstr.length = len;
+    tstr.pstrinfo = nullptr;
     tstr.ptr = buf;
     return tstr;
 }
 
-struct TString tstr_dup(struct TString tstr)
+struct TString tstr_new_ref_utf16(uint16_t const *buf TH_NONNULL, size_t len)
 {
-    struct TStringData *sh = to_heap(tstr);
-    if (!sh) {
-        return tstr_new(tstr.ptr, tstr.length);
+    struct TString tstr;
+    tstr.flags = TSTRING_REF | TSTRING_UTF16;
+    tstr.length = len * sizeof(uint16_t);
+    tstr.pstrinfo = nullptr;
+    tstr.ptr = reinterpret_cast<char const *>(buf);
+    return tstr;
+}
+
+struct TString tstr_new_from_external(char const *buf TH_NONNULL, size_t len, void *external_obj, void (*drop)(void *))
+{
+    struct TString tstr;
+    struct TStringInfo *info = reinterpret_cast<struct TStringInfo *>(malloc(sizeof(struct TStringInfo)));
+    if (!info) {
+        tstr.flags = 0;
+        tstr.length = 0;
+        tstr.pstrinfo = nullptr;
+        tstr.ptr = nullptr;
+        return tstr;
     }
-    tref_inc(&sh->count);
+    tref_init(&info->count, 1);
+    info->drop = drop;
+    info->external_obj = external_obj;
+
+    tstr.flags = TSTRING_UTF8 | TSTRING_EXT;
+    tstr.length = len;
+    tstr.pstrinfo = info;
+    tstr.ptr = buf;
+
+    return tstr;
+}
+
+struct TString tstr_new_from_external_utf16(uint16_t const *buf TH_NONNULL, size_t len, void *external_obj,
+                                            void (*drop)(void *))
+{
+    struct TString tstr;
+    struct TStringInfo *info = reinterpret_cast<struct TStringInfo *>(malloc(sizeof(struct TStringInfo)));
+    if (!info) {
+        tstr.flags = 0;
+        tstr.length = 0;
+        tstr.pstrinfo = nullptr;
+        tstr.ptr = nullptr;
+        return tstr;
+    }
+    tref_init(&info->count, 1);
+    info->drop = drop;
+    info->external_obj = external_obj;
+
+    tstr.flags = TSTRING_UTF16 | TSTRING_EXT;
+    tstr.length = len * sizeof(uint16_t);
+    tstr.pstrinfo = info;
+    tstr.ptr = reinterpret_cast<char const *>(buf);
+
+    return tstr;
+}
+
+struct TString tstr_dup(struct TString orig)
+{
+    // ref 需创建堆内存
+    // sh、external 不需要创建新的堆内存
+    if ((orig.flags & TSTRING_REF) == 0) {
+        tref_inc(&orig.pstrinfo->count);
+        return orig;
+    }
+    struct TString tstr;
+    size_t char_size = (orig.flags & TSTRING_UTF16) ? 2 : 1;
+    size_t bytes_required = sizeof(struct TStringInfo) + orig.length + char_size;
+    struct TStringInfo *newSh = reinterpret_cast<struct TStringInfo *>(malloc(bytes_required));
+    tref_init(&newSh->count, 1);
+    newSh->external_obj = nullptr;
+    newSh->drop = nullptr;
+
+    char *buffer = reinterpret_cast<char *>(newSh + 1);
+    std::copy(orig.ptr, orig.ptr + orig.length, buffer);
+    if (orig.flags & TSTRING_UTF16) {
+        uint16_t *dst = reinterpret_cast<uint16_t *>(buffer);
+        dst[orig.length / 2] = u'\0';
+    } else {
+        buffer[orig.length] = '\0';
+    }
+
+    tstr.flags = orig.flags & TSTRING_ENCODING_MASK;
+    tstr.length = orig.length;
+    tstr.pstrinfo = newSh;
+    tstr.ptr = buffer;
     return tstr;
 }
 
 void tstr_drop(struct TString tstr)
 {
-    struct TStringData *sh = to_heap(tstr);
+    if (tstr.flags & TSTRING_REF) {
+        return;
+    }
+    struct TStringInfo *sh = tstr.pstrinfo;
     if (!sh) {
         return;
     }
     if (tref_dec(&sh->count)) {
+        if ((tstr.flags & TSTRING_EXT) && sh->drop) {
+            sh->drop(sh->external_obj);
+        }
         free(sh);
     }
+}
+
+inline size_t utf8_to_utf16_required(char const *src, size_t len)
+{
+    uint8_t const *data = reinterpret_cast<uint8_t const *>(src);
+    size_t pos = 0, units = 0;
+
+    while (pos < len) {
+        if (pos + 16 <= len) {
+            uint64_t v1, v2;
+            std::copy(data + pos, data + pos + 8, reinterpret_cast<uint8_t *>(&v1));
+            std::copy(data + pos + 8, data + pos + 16, reinterpret_cast<uint8_t *>(&v2));
+            uint64_t v = v1 | v2;
+            if ((v & 0x8080808080808080) == 0) {
+                units += 16;
+                pos += 16;
+                continue;
+            }
+        }
+
+        uint8_t leading_byte = data[pos];
+        if (leading_byte < 0b10000000) {
+            // ASCII
+            units += 1;
+            pos++;
+        } else if ((leading_byte & 0b11100000) == 0b11000000) {
+            // 2 字节 UTF-8
+            if (pos + 1 >= len || (data[pos + 1] & 0b11000000) != 0b10000000) {
+                units += 1;
+                pos++;
+                continue;
+            }
+            units += 1;
+            pos += 2;
+        } else if ((leading_byte & 0b11110000) == 0b11100000) {
+            // 3 字节 UTF-8
+            if (pos + 2 >= len || (data[pos + 1] & 0b11000000) != 0b10000000 ||
+                (data[pos + 2] & 0b11000000) != 0b10000000) {
+                units += 1;
+                pos++;
+                continue;
+            }
+            units += 1;
+            pos += 3;
+        } else if ((leading_byte & 0b11111000) == 0b11110000) {
+            // 4 字节 UTF-8
+            if (pos + 3 >= len || (data[pos + 1] & 0b11000000) != 0b10000000 ||
+                (data[pos + 2] & 0b11000000) != 0b10000000 || (data[pos + 3] & 0b11000000) != 0b10000000) {
+                units += 1;
+                pos++;
+                continue;
+            }
+            units += 2;
+            pos += 4;
+        } else {
+            units += 1;
+            pos++;
+        }
+    }
+    return units;
+}
+
+inline size_t utf8_to_utf16(char const *input, size_t len, uint16_t *output)
+{
+    uint8_t const *data = reinterpret_cast<uint8_t const *>(input);
+    size_t pos = 0;
+    uint16_t *start {output};
+
+    while (pos < len) {
+        if (pos + 16 <= len) {
+            uint64_t v1, v2;
+            std::copy(data + pos, data + pos + 8, reinterpret_cast<uint8_t *>(&v1));
+            std::copy(data + pos + 8, data + pos + 16, reinterpret_cast<uint8_t *>(&v2));
+            uint64_t v {v1 | v2};
+            if ((v & 0x8080808080808080) == 0) {
+                size_t final_pos = pos + 16;
+                while (pos < final_pos) {
+                    *output++ = uint16_t(input[pos]);
+                    pos++;
+                }
+                continue;
+            }
+        }
+
+        uint8_t leading_byte = data[pos];
+        if (leading_byte < 0b10000000) {
+            // ASCII
+            *output++ = uint16_t(leading_byte);
+            pos++;
+        } else if ((leading_byte & 0b11100000) == 0b11000000) {
+            // 2 字节 UTF-8
+            if (pos + 1 >= len || (data[pos + 1] & 0b11000000) != 0b10000000) {
+                *output++ = 0xFFFD;
+                pos++;
+                continue;
+            }
+            uint32_t code_point = (leading_byte & 0b00011111) << 6 | (data[pos + 1] & 0b00111111);
+            if (code_point < 0x80 || 0x7ff < code_point) {
+                return 0;
+            }
+            *output++ = uint16_t(code_point);
+            pos += 2;
+        } else if ((leading_byte & 0b11110000) == 0b11100000) {
+            // 3 字节 UTF-8
+            if (pos + 2 >= len || (data[pos + 1] & 0b11000000) != 0b10000000 ||
+                (data[pos + 2] & 0b11000000) != 0b10000000) {
+                *output++ = 0xFFFD;
+                pos++;
+                continue;
+            }
+            uint32_t code_point =
+                (leading_byte & 0b00001111) << 12 | (data[pos + 1] & 0b00111111) << 6 | (data[pos + 2] & 0b00111111);
+            if (code_point < 0x800 || 0xffff < code_point || (0xd7ff < code_point && code_point < 0xe000)) {
+                return 0;
+            }
+            *output++ = uint16_t(code_point);
+            pos += 3;
+        } else if ((leading_byte & 0b11111000) == 0b11110000) {
+            // 4 字节 UTF-8
+            if (pos + 3 >= len || (data[pos + 1] & 0b11000000) != 0b10000000 ||
+                (data[pos + 2] & 0b11000000) != 0b10000000 || (data[pos + 3] & 0b11000000) != 0b10000000) {
+                *output++ = 0xFFFD;
+                pos++;
+                continue;
+            }
+            uint32_t code_point = (leading_byte & 0b00000111) << 18 | (data[pos + 1] & 0b00111111) << 12 |
+                                  (data[pos + 2] & 0b00111111) << 6 | (data[pos + 3] & 0b00111111);
+            if (code_point <= 0xffff || 0x10ffff < code_point) {
+                return 0;
+            }
+            code_point -= 0x10000;
+            uint16_t high_surrogate = uint16_t(0xD800 + (code_point >> 10));
+            uint16_t low_surrogate = uint16_t(0xDC00 + (code_point & 0x3FF));
+            *output++ = high_surrogate;
+            *output++ = low_surrogate;
+            pos += 4;
+        } else {
+            return 0;
+        }
+    }
+    return output - start;
+}
+
+inline size_t utf16_to_utf8_required(uint16_t const *src, size_t len)
+{
+    uint16_t const *data = src;
+    size_t pos = 0, units = 0;
+    while (pos < len) {
+        if (pos + 4 <= len) {
+            uint64_t v;
+            std::copy(data + pos, data + pos + 4, reinterpret_cast<uint8_t *>(&v));
+            if ((v & 0xFF80FF80FF80FF80) == 0) {
+                units += 4;
+                pos += 4;
+                continue;
+            }
+        }
+        uint16_t word = data[pos];
+        if ((word & 0xFF80) == 0) {
+            units += 1;
+            pos++;
+        } else if ((word & 0xF800) == 0) {
+            units += 2;
+            pos++;
+        } else if ((word & 0xF800) != 0xD800) {
+            units += 3;
+            pos++;
+        } else {
+            // must be a surrogate pair
+            uint16_t diff = uint16_t(word - 0xD800);
+            if (pos + 1 >= len || diff > 0x3FF) {
+                units += 3;
+                pos++;
+                continue;
+            }
+            if (uint16_t(data[pos + 1] - 0xDC00) > 0x3FF) {
+                units += 3;
+                pos++;
+                continue;
+            }
+            units += 4;
+            pos += 2;
+        }
+    }
+    return units;
+}
+
+inline size_t utf16_to_utf8(uint16_t const *input, size_t len, char *output)
+{
+    uint16_t const *data = input;
+    size_t pos = 0;
+    char *start {output};
+    while (pos < len) {
+        if (pos + 4 <= len) {
+            uint64_t v;
+            std::copy(data + pos, data + pos + 4, reinterpret_cast<uint8_t *>(&v));
+            if ((v & 0xFF80FF80FF80FF80) == 0) {
+                size_t final_pos = pos + 4;
+                while (pos < final_pos) {
+                    *output++ = char(input[pos]);
+                    pos++;
+                }
+                continue;
+            }
+        }
+        uint16_t word = data[pos];
+        if ((word & 0xFF80) == 0) {
+            *output++ = char(word);
+            pos++;
+        } else if ((word & 0xF800) == 0) {
+            *output++ = char((word >> 6) | 0b11000000);
+            *output++ = char((word & 0b111111) | 0b10000000);
+            pos++;
+        } else if ((word & 0xF800) != 0xD800) {
+            *output++ = char((word >> 12) | 0b11100000);
+            *output++ = char(((word >> 6) & 0b111111) | 0b10000000);
+            *output++ = char((word & 0b111111) | 0b10000000);
+            pos++;
+        } else {
+            // must be a surrogate pair
+            uint16_t diff = uint16_t(word - 0xD800);
+            if (pos + 1 >= len || diff > 0x3FF) {
+                *output++ = char(0xEF);
+                *output++ = char(0xBF);
+                *output++ = char(0xBD);
+                pos++;
+                continue;
+            }
+            uint16_t next_word = data[pos + 1];
+            uint16_t diff2 = uint16_t(next_word - 0xDC00);
+            if (diff2 > 0x3FF) {
+                *output++ = char(0xEF);
+                *output++ = char(0xBF);
+                *output++ = char(0xBD);
+                pos++;
+                continue;
+            }
+
+            uint32_t value = (diff << 10) + diff2 + 0x10000;
+            *output++ = char((value >> 18) | 0b11110000);
+            *output++ = char(((value >> 12) & 0b111111) | 0b10000000);
+            *output++ = char(((value >> 6) & 0b111111) | 0b10000000);
+            *output++ = char((value & 0b111111) | 0b10000000);
+            pos += 2;
+        }
+    }
+    return output - start;
+}
+
+struct TString tstr_utf8_to_utf16(struct TString utf8_str)
+{
+    if (tstr_encoding(utf8_str) == TSTRING_UTF16) return tstr_dup(utf8_str);
+
+    char const *src = tstr_buf(utf8_str);
+    size_t len = utf8_str.length;
+
+    size_t needed = utf8_to_utf16_required(src, len);
+
+    struct TString result;
+    uint16_t *dst = tstr_initialize_utf16(&result, (needed + 1));
+
+    if (!dst) return (struct TString) {.flags = TSTRING_UTF16, .length = 0, .pstrinfo = nullptr, .ptr = nullptr};
+
+    size_t used_len = utf8_to_utf16(src, len, dst);
+    dst[used_len] = u'\0';
+    result.flags = TSTRING_UTF16;
+    result.length = used_len * 2;
+    return result;
+}
+
+struct TString tstr_utf16_to_utf8(struct TString utf16_str)
+{
+    if (tstr_encoding(utf16_str) == TSTRING_UTF8) return tstr_dup(utf16_str);
+
+    uint16_t const *src = reinterpret_cast<uint16_t const *>(tstr_buf(utf16_str));
+    size_t len = utf16_str.length / 2;
+
+    size_t needed = utf16_to_utf8_required(src, len);
+
+    struct TString result;
+    char *dst = tstr_initialize(&result, (needed + 1));
+
+    if (!dst) return (struct TString) {.flags = TSTRING_UTF8, .length = 0, .pstrinfo = nullptr, .ptr = nullptr};
+
+    size_t used_len = utf16_to_utf8(src, len, dst);
+    dst[used_len] = '\0';
+    result.flags = TSTRING_UTF8;
+    result.length = used_len;
+    return result;
 }
 
 struct TString tstr_concat(size_t count, struct TString const *tstr_list)
@@ -100,6 +502,24 @@ struct TString tstr_concat(size_t count, struct TString const *tstr_list)
     return tstr;
 }
 
+struct TString tstr_concat_utf16(size_t count, struct TString const *tstr_list)
+{
+    size_t len = 0;
+    for (size_t i = 0; i < count; ++i) {
+        len += tstr_list[i].length;
+    }
+    size_t unit_code_len = len / 2;
+    struct TString tstr;
+    uint16_t *buf = tstr_initialize_utf16(&tstr, unit_code_len + 1);
+    for (size_t i = 0; i < count; ++i) {
+        buf = std::copy(reinterpret_cast<uint16_t const *>(tstr_list[i].ptr),
+                        reinterpret_cast<uint16_t const *>(tstr_list[i].ptr + tstr_list[i].length), buf);
+    }
+    *buf = u'\0';
+    tstr.length = len;
+    return tstr;
+}
+
 struct TString tstr_substr(struct TString tstr, size_t pos, size_t len)
 {
     if (pos > tstr.length) {
@@ -108,4 +528,15 @@ struct TString tstr_substr(struct TString tstr, size_t pos, size_t len)
         len = tstr.length - pos;
     }
     return tstr_new_ref(tstr.ptr + pos, len);
+}
+
+struct TString tstr_substr_utf16(struct TString tstr, size_t pos, size_t len)
+{
+    size_t unit_code_len = tstr.length / 2;
+    if (pos > unit_code_len) {
+        len = 0;
+    } else if (pos + len > unit_code_len) {
+        len = unit_code_len - pos;
+    }
+    return tstr_new_ref_utf16(reinterpret_cast<uint16_t const *>(tstr.ptr) + pos, len);
 }
