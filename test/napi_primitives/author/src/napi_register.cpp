2c3defa96ee1bb059cf44ad081034528f163773a
lyujiayi <lyujiayi@huawei.com>
2026-06-27 11:11:52 +0800

    test: fix function name mangling error
    Signed-off-by: lyujiayi <lyujiayi@huawei.com>

--- test/napi_primitives/author/src/napi_register.cpp
+++ test/napi_primitives/author/src/napi_register.cpp
@@ -23,7 +23,7 @@
 EXTERN_C_START
 napi_value Init(napi_env env, napi_value exports)
 {
-    Init__primitives_test(env, exports);
+    primitives_test::NapiInit(env, exports);
     return exports;
 }
 
