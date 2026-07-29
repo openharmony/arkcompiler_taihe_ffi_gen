61d49c02ba487c8f2747e11f4f31784ef2c0f39e
Jemtaly <Jemtaly@outlook.com>
2026-07-29 19:16:06 +0800

    test/napi: add set and union tests
    Issue: https://gitcode.com/openharmony/arkcompiler_taihe_ffi_gen/issues/158
    
    Co-Authored-By: Agent
    
    Signed-off-by: Jemtaly <Jemtaly@outlook.com>

--- test/napi_union/author/src/union_test.impl.cpp
+++ test/napi_union/author/src/union_test.impl.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -22,34 +22,72 @@ namespace {
 ::taihe::expected<::taihe::string, ::taihe::error> printUnion(::union_test::union_primitive const &data)
 {
     switch (data.get_tag()) {
-        case ::union_test::union_primitive::tag_t::sValue:
-            std::cout << "s: " << data.get_sValue_ref() << std::endl;
-            return "s";
+        case ::union_test::union_primitive::tag_t::stringValue:
+            std::cout << "string: " << data.get_stringValue_ref() << std::endl;
+            return "string";
         case ::union_test::union_primitive::tag_t::numberValue:
             std::cout << "number: " << (int)data.get_numberValue_ref() << std::endl;
             return "number";
-        case ::union_test::union_primitive::tag_t::bValue:
-            std::cout << "bool: " << data.get_bValue_ref() << std::endl;
-            return "bool";
-        case ::union_test::union_primitive::tag_t::aValue:
-            std::cout << "array: " << data.get_aValue_ref()[0] << std::endl;
+        case ::union_test::union_primitive::tag_t::booleanValue:
+            std::cout << "boolean: " << data.get_booleanValue_ref() << std::endl;
+            return "boolean";
+        case ::union_test::union_primitive::tag_t::arrayValue:
+            std::cout << "array: " << data.get_arrayValue_ref()[0] << std::endl;
             return "array";
-        case ::union_test::union_primitive::tag_t::mValue:
+        case ::union_test::union_primitive::tag_t::mapValue:
             std::cout << "map: " << std::endl;
-            for (auto const &[key, val] : data.get_mValue_ref()) {
+            for (auto const &[key, val] : data.get_mapValue_ref()) {
                 std::cout << "C++ Map: key: " << key << " value: " << val << std::endl;
             }
             return "map";
-        case ::union_test::union_primitive::tag_t::uValue:
+        case ::union_test::union_primitive::tag_t::setValue:
+            std::cout << "set: " << std::endl;
+            for (auto const &val : data.get_setValue_ref()) {
+                std::cout << "C++ Set: value: " << val << std::endl;
+            }
+            return "set";
+        case ::union_test::union_primitive::tag_t::int16arrayValue:
+            std::cout << "int16array: " << std::endl;
+            for (auto const &val : data.get_int16arrayValue_ref()) {
+                std::cout << "C++ Int16Array: value: " << val << std::endl;
+            }
+            return "int16array";
+        case ::union_test::union_primitive::tag_t::int32arrayValue:
+            std::cout << "int32array: " << std::endl;
+            for (auto const &val : data.get_int32arrayValue_ref()) {
+                std::cout << "C++ Int32Array: value: " << val << std::endl;
+            }
+            return "int32array";
+        case ::union_test::union_primitive::tag_t::arraybufferValue:
+            std::cout << "arraybuffer: " << std::endl;
+            for (auto const &val : data.get_arraybufferValue_ref()) {
+                std::cout << "C++ ArrayBuffer: value: " << static_cast<int>(val) << std::endl;
+            }
+            return "arraybuffer";
+        case ::union_test::union_primitive::tag_t::bigintValue:
+            std::cout << "bigint: " << std::endl;
+            for (auto const &val : data.get_bigintValue_ref()) {
+                std::cout << "C++ BigInt: value: " << val << std::endl;
+            }
+            return "bigint";
+        case ::union_test::union_primitive::tag_t::fooValue:
+            std::cout << "foo: " << std::endl;
+            std::cout << "C++ Foo: a: " << data.get_fooValue_ref().a << " b: " << data.get_fooValue_ref().b
+                      << std::endl;
+            return "foo";
+        case ::union_test::union_primitive::tag_t::barValue:
+            std::cout << "bar: " << std::endl;
+            return "bar";
+        case ::union_test::union_primitive::tag_t::undefinedValue:
             return "undefined";
-        case ::union_test::union_primitive::tag_t::nValue:
+        case ::union_test::union_primitive::tag_t::nullValue:
             return "null";
     }
 }
 
 ::taihe::expected<::union_test::union_primitive, ::taihe::error> makeUnion(::taihe::string_view kind)
 {
-    ::taihe::string s_value = "string";
+    ::taihe::string str_value = "string";
     constexpr double f64_value = 1.12345;
     constexpr bool bool_value = false;
     constexpr int32_t i32_value1 = 1;
@@ -58,32 +96,63 @@ namespace {
     ::taihe::map<int32_t, ::taihe::string> map_value;
     map_value.emplace(i32_value1, "a");
     map_value.emplace(i32_value2, "b");
+    ::taihe::set<::taihe::string> set_value;
+    set_value.emplace("a");
+    set_value.emplace("b");
+    ::taihe::array<int16_t> int16array_value = ::taihe::array<int16_t> {1, 2, 3};
+    ::taihe::array<int32_t> int32array_value = ::taihe::array<int32_t> {1, 2, 3};
+    ::taihe::array<uint8_t> arraybuffer_value = ::taihe::array<uint8_t> {1, 2, 3};
+    ::taihe::array<uint64_t> bigint_value = ::taihe::array<uint64_t> {1, 2, 3};
 
-    if (kind == "s") {
-        return ::union_test::union_primitive::make_sValue(s_value);
+    if (kind == "string") {
+        return ::union_test::union_primitive::make_stringValue(str_value);
     }
     if (kind == "number") {
         return ::union_test::union_primitive::make_numberValue(f64_value);
     }
-    if (kind == "bool") {
-        return ::union_test::union_primitive::make_bValue(bool_value);
+    if (kind == "boolean") {
+        return ::union_test::union_primitive::make_booleanValue(bool_value);
     }
     if (kind == "array") {
-        return ::union_test::union_primitive::make_aValue(array_value);
+        return ::union_test::union_primitive::make_arrayValue(array_value);
     }
     if (kind == "map") {
-        return ::union_test::union_primitive::make_mValue(map_value);
+        return ::union_test::union_primitive::make_mapValue(map_value);
     }
-    if (kind == "undefined") {
-        return ::union_test::union_primitive::make_uValue();
+    if (kind == "set") {
+        return ::union_test::union_primitive::make_setValue(set_value);
+    }
+    if (kind == "int16array") {
+        return ::union_test::union_primitive::make_int16arrayValue(int16array_value);
+    }
+    if (kind == "int32array") {
+        return ::union_test::union_primitive::make_int32arrayValue(int32array_value);
+    }
+    if (kind == "arraybuffer") {
+        return ::union_test::union_primitive::make_arraybufferValue(arraybuffer_value);
+    }
+    if (kind == "bigint") {
+        return ::union_test::union_primitive::make_bigintValue(bigint_value);
     }
     if (kind == "null") {
-        return ::union_test::union_primitive::make_nValue();
+        return ::union_test::union_primitive::make_nullValue();
     }
-    return ::union_test::union_primitive::make_uValue();
+    return ::union_test::union_primitive::make_undefinedValue();
+}
+
+::taihe::expected<::union_test::Foo, ::taihe::error> makeFoo(int a, ::taihe::string_view b)
+{
+    return ::union_test::Foo {a, b};
+}
+
+::taihe::expected<::union_test::Bar, ::taihe::error> makeBar()
+{
+    return taihe::make_holder<std::tuple<>, ::union_test::Bar>();
 }
 }  // namespace
 
 TH_EXPORT_CPP_API_printUnion(printUnion);
 TH_EXPORT_CPP_API_makeUnion(makeUnion);
+TH_EXPORT_CPP_API_makeFoo(makeFoo);
+TH_EXPORT_CPP_API_makeBar(makeBar);
 // NOLINTEND
